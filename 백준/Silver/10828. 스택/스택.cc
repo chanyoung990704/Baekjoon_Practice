41726c3183e1b6531327002bae2a3839222f87cb
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2023-08-03 00:04:14 +0900

    [Silver IV] Title: 스택, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,97 +1,81 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+
 using namespace std;
-
-
-class arrStack {
-private:
-	int* arr;
-	int capacity;
-	int t;
-
-public:
-	arrStack(int capacity) {
-		this->capacity = capacity;
-		this->t = -1;
-		this->arr = new int[capacity];
-	}
-
-	int size() {
-		return t + 1;
-	}
-
-	bool empty() {
-		return t == -1;
-	}
-
-	int top() {
-		if (empty())
-			return -1;
-		else
-			return arr[t];
-	}
-
-	void push(int e)
-	{
-		if (size() == capacity)
-			return;
-		else
-		{
-			t = t + 1;
-			arr[t] = e;
-		}
-	}
-
-	void pop() {
-		if (empty())
-			cout << "-1" << endl;
-		else
-		{
-			cout << arr[t] << endl;
-			t = t - 1;
-		}
-	}
-
-};
-
-
 
 
 
 int main() {
-	int N;
-	cin >> N;
 
-	arrStack arrStack(N);
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int N; cin >> N;
+
+	stack<int> numStack;
+
 
 	for (int i = 0; i < N; i++)
 	{
-		string op;
-		cin >> op;
+		string op;	cin >> op;
+
 
 		if (op == "push")
 		{
-			int val;
-			cin >> val;
-			arrStack.push(val);
+			int num;	cin >> num;
+			numStack.push(num);
+		}
+
+		if (op == "pop")
+		{
+			if (numStack.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << numStack.top() << "\n";
+				numStack.pop();
+			}
+		}
+
+		if (op == "size")
+		{
+			cout << numStack.size() << "\n";
+		}
+
+
+		if (op == "empty")
+		{
+			if (numStack.empty())
+				cout << 1 << "\n";
+			else
+				cout << 0 << "\n";
 		}
 
 		if (op == "top")
-			cout << arrStack.top() << endl;
-
-		if (op == "size")
-			cout << arrStack.size() << endl;
-
-		if (op == "pop")
-			arrStack.pop();
-		if (op == "empty")
 		{
-			if (arrStack.empty())
-				cout << "1" << endl;
+
+			if (numStack.empty())
+				cout << -1 << "\n";
 			else
-				cout << "0" << endl;
+				cout << numStack.top() << "\n";
+
+
 		}
+
+
 
 
 	}
 
+
+
+
 }
+
