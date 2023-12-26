5b46d1d7fca538ce658badab741a902ddc84fbb8
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2023-12-26 14:37:46 +0900

    [Gold II] Title: 가운데를 말해요, Time: 28 ms, Memory: 2840 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -1,33 +1,34 @@
-#include <iostream>
-#include <queue>
-#include <vector>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
 
 using namespace std;
 
-priority_queue<int> maxHeap; // 최대 힙
-priority_queue<int, vector<int>, greater<int>> minHeap; // 최소 힙
+priority_queue<int> max_pq; // 최대 힙
+priority_queue<int, vector<int>, greater<int>> min_pq; // 최소 힙
 
-void addNumber(int number) {
-    if (maxHeap.empty() || number <= maxHeap.top()) {
-        maxHeap.push(number);
+void calMedian(int val) {
+    if (max_pq.empty() || val <= max_pq.top()) {
+        max_pq.push(val);
     }
     else {
-        minHeap.push(number);
+        min_pq.push(val);
     }
 
-    // 힙의 크기 조절
-    if (maxHeap.size() > minHeap.size() + 1) {
-        minHeap.push(maxHeap.top());
-        maxHeap.pop();
+    if (max_pq.size() > min_pq.size() + 1) {
+        min_pq.push(max_pq.top());
+        max_pq.pop();
     }
-    else if (minHeap.size() > maxHeap.size()) {
-        maxHeap.push(minHeap.top());
-        minHeap.pop();
+    else if (min_pq.size() > max_pq.size()) {
+        max_pq.push(min_pq.top());
+        min_pq.pop();
     }
+
 }
 
 int getMedian() {
-    return maxHeap.top();
+    return max_pq.top();
 }
 
 int main() {
@@ -35,15 +36,11 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N;
-    cin >> N;
+    int N;	cin >> N;
 
     for (int i = 0; i < N; i++) {
-        int val;
-        cin >> val;
-        addNumber(val);
+        int val;	cin >> val;
+        calMedian(val);
         cout << getMedian() << "\n";
     }
-
-    return 0;
 }
