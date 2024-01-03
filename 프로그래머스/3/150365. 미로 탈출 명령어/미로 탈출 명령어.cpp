0cca3df130f70ba1bfaf54f3b97c9dcadb9dce4e
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-01-03 11:14:37 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2126.78 ms, Memory: 6.77 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,38 +1,56 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include <queue>
+
 using namespace std;
 
-char direction[4] = {'d', 'l', 'r', 'u'};
-int dy[4] = {1, 0, 0, -1};
-int dx[4] = {0, -1, 1, 0};
+char direction[4] = { 'd', 'l', 'r', 'u' };
+int dy[4] = { 1, 0, 0, -1 };
+int dx[4] = { 0, -1, 1, 0 };
+
+string BFS(const int& n, const int& m, int x, int y, const int& r, const int& c, const int& k) {
+
+	vector<vector<vector<bool>>> visited(n, vector<vector<bool>>(m, vector<bool>(k + 1, false)));
+
+	queue<tuple<int, int, string, int>> q;
+	q.push(make_tuple(x - 1, y - 1, "", 0));
+	visited[x - 1][y - 1][0] = true;
+
+	while (!q.empty()) {
+		int curY = get<0>(q.front());
+		int curX = get<1>(q.front());
+		string path = get<2>(q.front());
+		int cnt = get<3>(q.front());
+
+		q.pop();
+
+		// 도착했으면 return
+		if (curY == r - 1 && curX == c - 1 && cnt == k) {
+			return path;
+		}
+
+		// 하좌우상 탐색
+		for (int i = 0; i < 4; i++) {
+			int nextY = curY + dy[i];
+			int nextX = curX + dx[i];
+			string nextPath = path + direction[i];
+			int nextCnt = cnt + 1;
+			
+			if (nextY >= 0 && nextY < n && nextX >= 0 && nextX < m && nextCnt <= k && !visited[nextY][nextX][nextCnt]) {
+				q.push(make_tuple(nextY, nextX, nextPath, nextCnt));
+				visited[nextY][nextX][nextCnt] = true;
+			}
+
+		}
+	}
+
+	return "impossible";
+
+}
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    vector<vector<vector<bool>>> visited(n, vector<vector<bool>>(m, vector<bool>(k + 1, false)));
-    queue<pair<pair<int, int>, pair<string, int>>> q;
-    q.push({{x - 1, y - 1}, {"", 0}});
-    visited[x - 1][y - 1][0] = true;
-
-    while (!q.empty()) {
-        int curY = q.front().first.first;
-        int curX = q.front().first.second;
-        string path = q.front().second.first;
-        int count = q.front().second.second;
-        q.pop();
-
-        if (curY == r - 1 && curX == c - 1 && count == k) {
-            return path;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int newY = curY + dy[i];
-            int newX = curX + dx[i];
-            int newCount = count + 1;
-            if (newY >= 0 && newY < n && newX >= 0 && newX < m && newCount <= k && !visited[newY][newX][newCount]) {
-                q.push({{newY, newX}, {path + direction[i], newCount}});
-                visited[newY][newX][newCount] = true;
-            }
-        }
-    }
-    return "impossible";
+    string answer = BFS(n,m,x,y,r,c,k);
+    return answer;
 }
