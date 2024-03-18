c088dd2a8a446d8c26a4ec740dad8ac8014dc062
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-03-19 00:25:44 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,30 +1,38 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-bool dfs(vector<vector<string>>& tickets, vector<string>& answer, vector<bool>& visited, string start, int cnt) {
-    answer.push_back(start);
-
-    if (cnt == 0) return true;
-
-    for (int i = 0; i < tickets.size(); i++) {
-        if (!visited[i] && tickets[i][0] == start) {
+// dfs
+bool dfs(vector<vector<string>>& tickets, vector<string>& result, vector<bool>& visited, string cur) {
+    
+    if(result.size() == tickets.size() + 1)
+        return true;
+    
+    for(int i = 0 ; i < tickets.size() ; i++) {
+        if(!visited[i] && tickets[i][0] == cur) {
             visited[i] = true;
-            string dest = tickets[i][1];
-            if (dfs(tickets, answer, visited, dest, cnt - 1)) return true;
+            result.push_back(tickets[i][1]);
+            if(dfs(tickets, result, visited, tickets[i][1]))
+                return true;
+            result.pop_back();
             visited[i] = false;
         }
     }
-
-    answer.pop_back();
+    
     return false;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
-    vector<string> answer;
-    vector<bool> visited(tickets.size(), false); // 티켓 사용 여부를 저장하는 visited 배열
+    
+    vector<string> result;
+    result.push_back("ICN");
+    vector<bool> visited(tickets.size(), false);
+    
     sort(tickets.begin(), tickets.end());
-    dfs(tickets, answer, visited, "ICN", tickets.size());
-    return answer;
+    
+    dfs(tickets, result, visited, "ICN");
+    
+    return result;
 }
