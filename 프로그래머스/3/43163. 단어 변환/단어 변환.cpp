7a2e9c2fca15d51410a8c97b87b4e85214f860eb
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-03-18 15:48:12 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,47 +1,58 @@
+#include <iostream>
+#include <string>
 #include <vector>
+#include <unordered_set>
 #include <queue>
 #include <tuple>
+#include <map>
 
 using namespace std;
 
-bool isChange(const string& a, const string& b) {
-    int cnt = 0;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            cnt++;
-        if (cnt > 1)
-            return false;
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+
+    // set으로 대체
+    unordered_set<string> us(wordList.begin(), wordList.end());
+    // 단어가 존재하지 않을 경우
+    if (us.find(endWord) == us.end()) {
+        return 0;
     }
 
-    return cnt == 1;
-}
-
-
-int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
+    // 단어가 존재할 경우 bfs
+    map<string, bool> visited;
     queue<tuple<string, int>> q;
-    vector<bool> visited(words.size(), false);
-    q.push(make_tuple(begin, 0));
+    visited[beginWord] = true;
+    q.push(make_tuple(beginWord, 0));
 
     while (!q.empty()) {
-        string curWord = get<0>(q.front());
-        int cnt = get<1>(q.front());
+        string curWord = get<0>(q.front()); int cnt = get<1>(q.front());
         q.pop();
 
-        if (curWord == target)
+        if (curWord == endWord)
             return cnt;
 
-        for (int i = 0; i < words.size(); i++) {
-            string word = words[i];
-            if (isChange(word, curWord) && !visited[i]) {
-                q.push(make_tuple(word, cnt + 1));
-                visited[i] = true;
+        for (int i = 0; i < wordList.size(); i++) {
+            // 방문하지 않고
+            if (!visited[wordList[i]]) {
+                int diff = 0;
+                // 한글자만 다르다면 큐에 집어넣는다.
+                for (int j = 0; j < curWord.size(); j++) {
+                    if (curWord[j] != wordList[i][j])
+                        diff++;
+                }
+                if (diff == 1) {
+                    visited[wordList[i]] = true;
+                    q.push(make_tuple(wordList[i], cnt + 1));
+                }
             }
-
         }
-
 
     }
 
-    return answer;
+    return 0;
+}
+
+int solution(string begin, string target, vector<string> words) {
+
+    int ret = ladderLength(begin, target, words);
+    return ret;
 }
