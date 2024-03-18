398c81b2bc2d3775951e174d30e3af0204e8f531
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-03-18 13:55:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.38 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,49 +1,55 @@
-#include <algorithm>
-#include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 #include <tuple>
-
 using namespace std;
 
-int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = {0, 0, 1, -1};
+int dx[4] = {1, -1, 0, 0};
 
-int bfs(vector<vector<int>>& maps, vector<vector<bool>>& visited, int n, int m) {
-    queue<tuple<int, int, int>> q; // 튜플에 거리 정보 추가
+int bfs(vector<vector<int>>& map) {
+    // 방문 배열
+    vector<vector<bool>> visited(map.size(),
+                                 vector<bool>(map[0].size(), false));
     visited[0][0] = true;
-    q.push(make_tuple(0, 0, 1)); // 시작점과 거리 1로 초기화
-
-    while (!q.empty()) {
-        int curY = get<0>(q.front());
-        int curX = get<1>(q.front());
-        int dist = get<2>(q.front());
+    // 큐
+    queue<tuple<int,int,int>> q;
+    // 시작 좌표 0,0 그리고 cnt
+    q.push(make_tuple(0, 0, 1));
+    
+    while(!q.empty()) {
+        
+        int curY = get<0>(q.front());   int curX = get<1>(q.front());
+        int cnt = get<2>(q.front());
         q.pop();
-
-        if (curY == n - 1 && curX == m - 1) // 목적지 도착 시 거리 반환
-            return dist;
-
-        for (int i = 0; i < 4; i++) {
-            int nextY = curY + dy[i];
-            int nextX = curX + dx[i];
-
-            if (nextY >= 0 && nextY < n && nextX >= 0 && nextX < m
-                && !visited[nextY][nextX] && maps[nextY][nextX] == 1) {
-                visited[nextY][nextX] = true;
-                q.push(make_tuple(nextY, nextX, dist + 1)); // 거리 정보 업데이트
+        
+        // baseCase
+        if(curY == map.size() - 1 && curX == map[0].size() - 1)
+            return cnt;
+        
+        // 상하좌우 이동
+        for(int i = 0 ; i < 4 ; i++) {
+            int nextY = curY + dy[i];   int nextX = curX + dx[i];
+            // 좌표 안일 경우
+            if(nextY >= 0 && nextY < map.size() && 
+              nextX >= 0 && nextX < map[0].size()) {
+                // 방문을 하지 않았을 경우 그리고 1인 경우
+                if(!visited[nextY][nextX] && map[nextY][nextX] == 1) {
+                    visited[nextY][nextX] = true;
+                    q.push(make_tuple(nextY, nextX, cnt + 1));
+                }
             }
+            
         }
+        
     }
-
-    return -1; // 목적지에 도달할 수 없는 경우 -1 반환
+    
+    
+    return -1;
 }
 
 int solution(vector<vector<int> > maps)
 {
-    int answer = 0;
-    int n = maps.size();
-    int m = maps[0].size();
-    vector<vector<bool>> visited(n, vector<bool>(m, false));
-    answer = bfs(maps, visited, n, m);
-    return answer;
+
+    return bfs(maps);
 }
