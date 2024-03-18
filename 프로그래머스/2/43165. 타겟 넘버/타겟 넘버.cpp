4bcc47361367ce67859ce1dc9d24f3ead3ee8f1a
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-03-18 11:44:28 +0900

    [level 2] Title: 타겟 넘버, Time: 0.07 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,22 +1,38 @@
-#include <algorithm>
-#include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int dfs(const vector<int>& numbers, const int& target, int result, int cnt) {
-	// baseCase
-	if (cnt == numbers.size())
-		if (result == target)
-			return 1;
-		else
-			return 0;
-
-	return dfs(numbers, target, result + numbers[cnt], cnt + 1) + dfs(numbers, target, result - numbers[cnt], cnt + 1);
-
+int dfs(vector<int>& numbers, vector<vector<bool>>& visited, int target, int cur, int curNum) {
+    // Base case
+    if(cur == numbers.size()) {
+        return curNum == target ? 1 : 0;
+    }
+    
+    int ret = 0;
+    // 음수일 경우
+    if(!visited[cur][0]) {
+        visited[cur][0] = true;
+        ret += dfs(numbers, visited, target, cur + 1, curNum - numbers[cur]);
+        visited[cur][0] = false;
+    }
+    
+    // 양수일 경우
+    if(!visited[cur][1]) {
+        visited[cur][1] = true;
+        ret += dfs(numbers, visited, target, cur + 1, curNum + numbers[cur]);
+        visited[cur][1] = false;
+    }
+    
+    return ret;
+    
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = dfs(numbers, target, 0, 0);
-    return answer;
+    // [원소 idx][0] -> 음수, [원소 idx][1] -> 양수
+    vector<vector<bool>> visited(numbers.size(), vector<bool>(2, false));
+    int result = 0;
+    result = dfs(numbers, visited, target, 0, 0);
+    return result;
 }
