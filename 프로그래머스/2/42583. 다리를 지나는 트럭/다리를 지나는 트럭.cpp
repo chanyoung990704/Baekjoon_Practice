286ea811b3e7f2fc96209da4dcd6b2f4a5c81054
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-03-20 13:15:59 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -5,34 +5,37 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    int total_weight = 0;
-    queue<int> q;
-
-    // 트럭이 다리를 건너는 시간을 나타내는 변수
-    vector<int> time(truck_weights.size(), 0);
-
+    
+    int totalTime = 0; // 경과시간
+    vector<int> truckIn(truck_weights.size(), 0); // 트럭이 처음 다리에 진입한 시간
+    int totalWeight = 0; // 현재 다리에 트럭 무게
+    queue<int> q; // 다리에 있는 트럭 무게
+    
     int idx = 0;
-    while (idx < truck_weights.size()) {
-        answer++; // 시간 증가
-
-        // 다리의 맨 앞에 있는 트럭이 다리를 건넌 경우
-        if (!q.empty() && answer - time[q.front()] >= bridge_length) {
-            total_weight -= truck_weights[q.front()]; // 다리 위 트럭의 무게 감소
-            q.pop(); // 다리에서 트럭 제거
+    while(idx < truck_weights.size()) {
+        // 경과 시간 증가
+        totalTime++;
+        
+        // 다리에 차가 빠져나갈 수 있으면
+        if(!q.empty() && totalTime - truckIn[q.front()] >= bridge_length) {
+            // 무게 감소
+            totalWeight -= truck_weights[q.front()];
+            q.pop();
         }
-
-        // 현재 다리에 올릴 수 있는 경우
-        if (total_weight + truck_weights[idx] <= weight) {
-            q.push(idx); // 트럭 추가
-            total_weight += truck_weights[idx]; // 다리 위 트럭의 무게 증가
-            time[idx] = answer; // 트럭이 다리에 올라간 시간 기록
-            idx++; // 다음 트럭으로 이동
+        
+        // 다리에 차가 진입할 수 있으면
+        if(totalWeight + truck_weights[idx] <= weight) {
+            // 무게 증가
+            totalWeight += truck_weights[idx];
+            // 큐에 삽입
+            q.push(idx);
+            // 시간 업데이트
+            truckIn[idx] = totalTime;
+            // 다음 트럭으로 이동
+            idx++;
         }
     }
-
-    // 마지막 트럭이 다리를 지날 때까지의 시간에 다리의 길이를 더해준다.
-    answer += bridge_length;
-
-    return answer;
+    
+    // 마지막 트럭 시간 고려
+    return totalTime + bridge_length;
 }
