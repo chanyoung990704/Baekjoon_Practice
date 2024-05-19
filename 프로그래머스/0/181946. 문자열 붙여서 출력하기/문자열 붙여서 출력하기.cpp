efada33fbd85a37653dfa4d23ce8621f44d348ee
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-05-19 15:09:34 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 2.77 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    string result;
+    result = str1 + str2;
+    cout << result;
+    return 0;
+}
