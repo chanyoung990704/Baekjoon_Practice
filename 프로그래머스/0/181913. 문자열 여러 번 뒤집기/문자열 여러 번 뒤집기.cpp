746d800a7eadae967c990064cc823e8595e50541
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-05-19 21:07:50 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.20 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string answer = "";
+    answer.append(my_string);
+    
+    for(auto query : queries){
+        reverse(answer.begin() + query[0], answer.begin() + query[1] + 1);
+    }
+    return answer;
+}
