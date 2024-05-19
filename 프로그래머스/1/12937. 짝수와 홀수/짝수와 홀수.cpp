83f3e8090b2317097d133350d00b863204f68b69
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-05-20 00:46:43 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    if(num % 2 == 0)
+        answer.append("Even");
+    else
+        answer.append("Odd");
+    
+    return answer;
+}
