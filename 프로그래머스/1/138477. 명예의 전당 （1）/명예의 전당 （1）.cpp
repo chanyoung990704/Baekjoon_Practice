34d16a4a085b5d9faef380f2a47d37f28315a8a7
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-05-20 17:07:33 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    
+    priority_queue<int, vector<int>, greater<int>> q;
+    for(int i = 0 ; i < score.size() ; i++){
+        q.push(score[i]);
+        
+        if(q.size() == k + 1){
+            q.pop();
+        }
+        
+        answer.push_back(q.top());
+        
+    }
+    
+    return answer;
+}
