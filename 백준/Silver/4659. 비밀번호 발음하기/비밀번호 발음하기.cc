ee2cc668992918cbbb0b9b075ff8c70e9cdd7339
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-05-29 21:42:48 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,93 +1,60 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-bool isPassword(string password) {
-
-	// ee를 e로 변환
-	size_t pos = password.find("ee");
-	while (pos != string::npos)
-	{
-		password.replace(pos, 2, "e");
-		pos = password.find("ee", pos + 1);
-	}
-
-	//oo를 o로 변환
-	pos = password.find("oo");
-	while (pos != string::npos)
-	{
-		password.replace(pos, 2, "o");
-		pos = password.find("oo", pos + 1);
-	}
-
-	bool prevStatus = false; // false : 자음, true : 모음
-	char prevChar = ' '; // 이전 단어
-	int cnt = 0; // 연속 자음 / 모음 개수
-	bool isM = false; // 모음 있는지 확인
-
-	for (int i = 0; i < password.size(); i++) {
-		if (cnt >= 3)
-			return false;
-		
-		if (prevChar == password[i])
-			return false;
-
-		// 모음인 경우
-		if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || password[i] == 'o' || password[i] == 'u') {
-			isM = true;
-			// 이전 단어가 자음인경우
-			if (!prevStatus) {
-				prevStatus = true;
-				cnt = 1;
-			}
-			else {
-				cnt++;
-			}
-		}
-
-		// 자음인 경우
-		else {
-			// 이전 단어가 자음인 경우
-			if (!prevStatus)
-				cnt++;
-			else {
-				prevStatus = false;
-				cnt = 1;
-			}
-
-		}
-
-		// 이전 단어 갱신
-		prevChar = password[i];
-
-	}
-
-	if (!isM || cnt >= 3)
-		return false;
-
-	return true;
-
+bool isVowel(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    while (true) {
+        string s;
+        cin >> s;
 
-	while (true)
-	{
-		string password;	cin >> password;
+        if (s == "end")
+            break;
 
-		if (password == "end")
-			break;
+        char prev = '\0'; // 초기화
+        int s_idx = 0;
+        bool acceptable = true;
+        bool has_vowel = false;
+        int vowel_cnt = 0;
+        int not_vowel_cnt = 0;
 
-		if (isPassword(password))
-			cout << "<" << password << "> is acceptable.\n";
-		else
-			cout << "<" << password << "> is not acceptable.\n";
-	}
+        while (s_idx < s.size()) {
+            char cur = s[s_idx];
+            if (isVowel(cur)) {
+                has_vowel = true;
+                vowel_cnt++;
+                not_vowel_cnt = 0;
+            } else {
+                vowel_cnt = 0;
+                not_vowel_cnt++;
+            }
 
+            if (s_idx > 0 && prev == cur && cur != 'e' && cur != 'o') // 첫 번째 문자는 비교하지 않도록 수정
+                acceptable = false;
+
+            if (vowel_cnt == 3 || not_vowel_cnt == 3)
+                acceptable = false;
+
+            prev = cur;
+            s_idx++;
+        }
+
+        if (!has_vowel)
+            acceptable = false;
+
+        if (acceptable)
+            cout << "<" << s << "> is acceptable.\n";
+        else
+            cout << "<" << s << "> is not acceptable.\n";
+    }
 }
