5794256e895f4e4368f058c2d63e93ceeeeec5aa
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-06-03 17:33:07 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057,) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 4.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 23일 01:20:09
+2024년 06월 03일 17:33:06
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -5,35 +5,45 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = s.size();
+    int answer = s.size();  // 초기 값을 문자열의 길이로 설정
     
-    for(int step = 1 ; step <= (s.size() / 2) ; step++){
-        string compressed = "";
-        string prev = s.substr(0, step);
+    for(int i = 1 ; i <= s.size() / 2 ; i++) {
+        
+        string prev = s.substr(0, i);  // 첫 번째 부분 문자열 초기화
+        string result = "";
+        size_t s_idx = i;
         int cnt = 1;
         
-        for(int j = step ; j < s.size() ; j += step){
-            string sub_s = s.substr(j, step);
-            if(sub_s == prev)
-                cnt += 1;
-            else{
-                if(cnt > 1)
-                    compressed.append(to_string(cnt));
-                compressed.append(prev);
-                prev = sub_s;
-                cnt = 1;
-            }
+        while(s_idx < s.size()) {
+            // 문자열 분해
+            string tmp = s.substr(s_idx, i);
+            
+            if(prev != tmp) {
+                if(cnt > 1){
+                    result.append(to_string(cnt));
+                }
+                result.append(prev);
                 
+                cnt = 1;
+                prev = tmp;
+                
+            } else {
+                cnt++;
+            }
+            
+            s_idx += i;
         }
         
-        if(cnt > 1)
-            compressed.append(to_string(cnt));
-        compressed.append(prev);
+        // 마지막 부분 처리
+        if(cnt > 1) {
+            result.append(to_string(cnt));
+        }
+        result.append(prev);
         
-        answer = min(answer, (int)compressed.size());
-    }
-    
-    
+        // answer 갱신
+        int tmp_res = result.size();
+        answer = min(answer, tmp_res);
+    }    
     
     return answer;
 }
