0a4cbb7efd58ca6d64d5b822c7c7d408dc2ab592
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-06-21 12:52:48 +0900

    [level 2] Title: 하노이의 탑, Time: 23.57 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -3,16 +3,24 @@
 
 using namespace std;
 
-void hanoi(int n, int start, int mid, int end, vector<vector<int>>& answer) {
+void hanoi(int n, int start, int mid, int end,
+          vector<vector<int>>& answer) {
     
+    // 1개일 때 start -> end
     if(n == 1){
         answer.push_back(vector<int>{start, end});
         return;
     }
     
+    // n -1 개를 mid로
     hanoi(n - 1, start, end, mid, answer);
+    
+    // 1개 남은 것을 end로
     answer.push_back(vector<int>{start, end});
+    
+    // mid로 옮긴 n -1 개를 다시 end로
     hanoi(n - 1, mid, start, end, answer);
+    
     
 }
 
