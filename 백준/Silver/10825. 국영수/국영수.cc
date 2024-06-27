9019ae67d18d2aed0a300e007a2ac2ddd88641bb
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-06-27 16:35:44 +0900

    [Silver IV] Title: 국영수, Time: 56 ms, Memory: 6716 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.cc
+++ 백준/Silver/10825. 국영수/국영수.cc
@@ -2,37 +2,37 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#include <string>
 
 using namespace std;
 
-bool cmp(const tuple<string, int, int, int>& a, const tuple<string, int, int, int>& b) {
+bool cmp(tuple<string, int, int, int>& a, tuple<string, int, int, int>& b) {
     if(get<1>(a) != get<1>(b))
         return get<1>(a) > get<1>(b);
     if(get<2>(a) != get<2>(b))
         return get<2>(a) < get<2>(b);
     if (get<3>(a) != get<3>(b))
         return get<3>(a) > get<3>(b);
+
     return get<0>(a) < get<0>(b);
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
+    cout.tie(NULL);
     
-    int N;
-    cin >> N;
+    int N;  cin >> N;
 
     vector<tuple<string, int, int, int>> students(N);
-    for(auto& student : students) {
-        cin >> get<0>(student) >> get<1>(student) >> get<2>(student) >> get<3>(student);
-    }
+    for(int i = 0 ; i < N ; i++)
+        cin >> get<0>(students[i]) >> get<1>(students[i]) >>
+        get<2>(students[i]) >> get<3>(students[i]);
     
     sort(students.begin(), students.end(), cmp);
 
-    for(const auto& student : students) {
-        cout << get<0>(student) << '\n';
-    }
+    for(auto& s : students)
+        cout << get<0>(s) << "\n";
 
-    return 0;
+    
+
 }
