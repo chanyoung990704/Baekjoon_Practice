10c342c417be5d558fe3f0192593793e6fdea5e9
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-07-08 17:02:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.66 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,9 @@
+import java.util.stream.Stream;
+
+class Solution {
+    public int solution(String my_string) {
+        return Stream.of(my_string.replaceAll("[^0-9]", "").split(""))
+                     .mapToInt(Integer::parseInt)
+                     .sum();
+    }
+}
