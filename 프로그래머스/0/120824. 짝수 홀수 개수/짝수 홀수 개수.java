2b91892eea3a686f63f2e7bd45d4727f455c4c46
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-07-08 22:18:36 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int odd = 0;
+        int even = 0;
+        
+        for(var i : num_list){
+            if(i % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        
+        return new int[]{even, odd};
+    }
+}
