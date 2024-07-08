abfcc40bc7fdcd26c6a95ac4c2f1ea0784f935ae
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-07-08 23:44:56 +0900

    [level 0] Title: 주사위의 개수, Time: 1.40 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] box, int n) {
+        
+        return Arrays.stream(box)
+            .map(i -> i / n)
+            .reduce(1, (a, b) -> a * b);
+                
+    }
+}
