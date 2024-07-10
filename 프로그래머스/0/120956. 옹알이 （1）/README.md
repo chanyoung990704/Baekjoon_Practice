b63f1693e48aec6a138ac4aa871ff443739085c1
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-07-10 14:32:19 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/README.md
+++ 프로그래머스/0/120956. 옹알이 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옹알이 (1) - 120956 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=java) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 76.8 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 25일 01:23:32
+2024년 07월 10일 14:32:21
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        
+        String[] canBabbling = new String[]{"aya", "ye", "woo", "ma"};
+        
+        int result = 0;
+        for(String cur : babbling) {            
+            for(String s : canBabbling){
+                if(cur.contains(s))
+                    cur = cur.replace(s, " ");
+            }
+            
+            if(cur.trim().isEmpty())
+                result++;
+        }
+        
+        
+        return result;
+    }
+}
