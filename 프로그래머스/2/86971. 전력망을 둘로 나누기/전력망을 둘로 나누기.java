68aced185212a4799f5b813dd783a72e752f6605
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-08-06 11:56:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.05 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,46 +1,51 @@
 import java.util.*;
-
 class Solution {
+    
     int[] parent;
     
-    public int solution(int n, int[][] wires) {
-        int answer = n;
-        parent = new int[n+1];
+    int findParent(int n){
+        if(n != parent[n]){
+            parent[n] = findParent(parent[n]);
+        }
+        return parent[n];
+    }
+    
+    void unionParent(int a, int b){
+        int rootA = findParent(a);
+        int rootB = findParent(b);
         
-        for (int i = 0; i < wires.length; i++) {
-            // Try cutting each wire
-            for (int j = 1; j <= n; j++) {
+        if(rootA < rootB){
+            parent[rootB] = rootA;
+        }else{
+            parent[rootA] = rootB;
+        }
+    }
+    
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+        parent = new int[n + 1];
+        
+        for(int i = 0 ; i < wires.length ; i++) {
+            for(int j = 1 ; j <= n ; j++){
                 parent[j] = j;
             }
             
-            for (int j = 0; j < wires.length; j++) {
-                if (i == j) continue; // Skip the cut wire
-                union(wires[j][0], wires[j][1]);
+            for(int j = 0 ; j < wires.length ; j++){
+                if(i == j) continue;
+                unionParent(wires[j][0], wires[j][1]);
             }
             
-            // Count the sizes of the two components
             int size1 = 0;
-            int root = find(1);
-            for (int j = 1; j <= n; j++) {
-                if (find(j) == root) size1++;
+            int root = findParent(1);
+            for(int j = 1 ; j <= n ; j++){
+                if(findParent(j) == root) size1++;
             }
             int size2 = n - size1;
             
-            // Update the answer
             answer = Math.min(answer, Math.abs(size1 - size2));
         }
         
+        
         return answer;
-    }
-    
-    private int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    private void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) parent[b] = a;
     }
 }
