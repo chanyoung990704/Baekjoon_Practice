255e9b2bc1f862d9a2a419e8c02c9943dd51df8e
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-08-07 14:38:57 +0900

    [level 3] Title: 순위, Time: 15.82 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,8 +1,5 @@
 import java.util.*;
-import java.util.stream.*;
-
 class Solution {
-    
     public int solution(int n, int[][] results) {
         int answer = 0;
         
@@ -12,37 +9,29 @@ class Solution {
             dist[i][i] = 0;
         }
         
-        // 간선 입력
-        for(int[] result : results){
-            dist[result[0]][result[1]] = 1;
-        }
+        for(int[] res : results) dist[res[0]][res[1]] = 1;
         
-        // 플루이드 워셜
         for(int i = 1 ; i <= n ; i++){
             for(int a = 1 ; a <= n ; a++){
                 for(int b = 1 ; b <= n ; b++){
                     if(dist[a][i] != Integer.MAX_VALUE &&
-                      dist[i][b] != Integer.MAX_VALUE){
-                        dist[a][b] = Math.min(dist[a][b], dist[a][i] + 
+                      dist[i][b] != Integer.MAX_VALUE) {
+                        dist[a][b] = Math.min(dist[a][b], dist[a][i] +
                                              dist[i][b]);
                     }
-                    
                 }
             }
         }
         
-        for(int i = 1 ; i <= n ; i++){
-            int count = 0;
-            for(int j = 1 ; j <= n ; j++){
-                if(i != j && (dist[i][j] != Integer.MAX_VALUE || 
-                             dist[j][i] != Integer.MAX_VALUE)){
-                    count++;
-                }
+        
+        for(int a = 1 ; a <= n ; a++){
+            int cnt = 0;
+            for(int b = 1 ; b <= n ; b++){
+                if(dist[a][b] != Integer.MAX_VALUE || 
+                  dist[b][a] != Integer.MAX_VALUE) cnt++;
             }
-            if(count == n - 1)
-                answer++;
+            if(cnt == n) answer++;
         }
-                   
         
         
         return answer;
