e7b063f6553fd7a66c75415def45737dd5484238
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-08-08 14:41:40 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 39.92 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,71 +1,46 @@
 import java.util.*;
 import java.util.stream.*;
-
 class Solution {
-    
-    class Food{
-        int cnt;
-        String cur;
-        
-        Food(int cnt, String cur){
-            this.cnt = cnt;
-            this.cur = cur;
-        }
-        int getCnt(){return cnt;}
-        String getCur(){return cur;}
-    }
-    
-    List<String> result = new ArrayList<>();
-    
     public String[] solution(String[] orders, int[] course) {
         
-        Set<Character> set = new TreeSet<>();
-        for(String order : orders)
-            for(char c : order.toCharArray()) set.add(c);
-        
-        String total = set.stream()
-            .map(i -> String.valueOf(i))
-            .collect(Collectors.joining());
+        List<String> result = new ArrayList<>();
                 
-        for(int c : course){
-            PriorityQueue<Food> pq = 
-                new PriorityQueue<>(Comparator.comparing(Food::getCnt).reversed());
-            comb(0, total, "", c, orders, pq);
+        for(int c : course) {
+            Map<String, Integer> combinationCount = 
+                new HashMap<>();
             
-            int max = 0;
-            if(!pq.isEmpty()) max = pq.peek().getCnt();
-            while(!pq.isEmpty() && max == pq.peek().getCnt()){
-                result.add(pq.poll().getCur());
+            for(String order : orders) {
+                char[] orderArr = order.toCharArray();
+                Arrays.sort(orderArr);
+                findCombinations(orderArr, c, 0, "", combinationCount);
             }
+            
+            int max = 2;
+            for(int curCount : combinationCount.values())
+                if(curCount > max) max = curCount;
+            
+            for(Map.Entry<String, Integer> cur : combinationCount.entrySet()){
+                if(cur.getValue() == max) result.add(cur.getKey());
+            }
+            
         }
+        
         
         return result.stream()
             .sorted()
             .toArray(String[]::new);
-        
     }
     
-    void comb(int idx, String s, String res, int c, String[] orders
-             ,PriorityQueue<Food> pq){
-        if(res.length() == c){
-            int cur = 0;
-            for(String order : orders){
-                boolean isPossible = true;
-                for(char ch : res.toCharArray()){
-                    String cs = String.valueOf(ch);
-                    if(!order.contains(cs)){
-                        isPossible = false;
-                        break;
-                    }
-                }
-                if(isPossible) cur++;
-            }
-            
-            if(cur >= 2) pq.offer(new Food(cur, res));
+    
+    private void findCombinations(char[] order, int len, int start,
+                String cur, Map<String, Integer> combinationCount) {
+        if(cur.length() == len){
+            combinationCount.put(cur, 
+                combinationCount.getOrDefault(cur, 0) + 1);
             return;
         }
-        
-        for(int i = idx ; i < s.length() ; i++)
-            comb(i + 1, s, res + String.valueOf(s.charAt(i)), c, orders, pq);
+        for(int i = start ; i < order.length ; i++)
+            findCombinations(order, len, i + 1, cur + order[i],
+                combinationCount);
     }
 }
