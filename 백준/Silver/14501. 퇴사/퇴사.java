2afdd9f3ac83bf89a852b648c12dac7496ce7ee9
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-08-14 17:50:16 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -12,23 +12,24 @@ public class Main {
         days.add(new int[]{-1, -1}); // 더미값
         for(int i = 1 ; i <= N ; i++) {
             int[] cur = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::valueOf).toArray();
+            .mapToInt(Integer::valueOf).toArray();
             days.add(cur);
         }
 
-        int[] dp = new int[N + 1];
+        int[] dp = new int[N + 2]; 
+        int max = 0;
         for(int i = N ; i > 0 ; i--){
             int[] cur = days.get(i);
-            if(i + cur[0] - 1 <= N) {
-                int nextDay = i + cur[0];
-                int profit = cur[1] + (nextDay <= N ? dp[nextDay] : 0);
-                dp[i] = Math.max(profit, (i < N ? dp[i+1] : 0));
-            } else {
-                dp[i] = (i < N ? dp[i+1] : 0);
+
+            // 오늘을 포함할 수 있는 경우
+            if(i + cur[0] <= N + 1) { 
+                max = Math.max(max, cur[1] + dp[i + cur[0]]); 
             }
+
+            dp[i] = max;
         }
 
-        System.out.println(dp[1]);
+        System.out.println(dp[1]); 
 
         br.close();
     }
