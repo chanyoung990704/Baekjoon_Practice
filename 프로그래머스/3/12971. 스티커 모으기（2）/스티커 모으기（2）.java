c55a23121e874ca5b25d0a7dc0f58093cc050fe4
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-09-04 12:51:08 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.37 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sticker) {
+        if (sticker.length == 1) return sticker[0];
+        if (sticker.length == 2) return Math.max(sticker[0], sticker[1]);
+
+        // 첫 번째 스티커를 선택하는 경우
+        int[] dp1 = new int[sticker.length];
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
+        for (int i = 2; i < sticker.length - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
+        }
+
+        // 첫 번째 스티커를 선택하지 않는 경우
+        int[] dp2 = new int[sticker.length];
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for (int i = 2; i < sticker.length; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+        }
+
+        return Math.max(dp1[sticker.length - 2], dp2[sticker.length - 1]);
+    }
+}
