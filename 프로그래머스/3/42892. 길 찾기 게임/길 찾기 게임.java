c3b0fa5d30b47bd810b853f6742d20820e85076a
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-10-02 11:26:04 +0900

    [level 3] Title: 길 찾기 게임, Time: 75.89 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -2,88 +2,61 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    class Node{
-        int x;
-        int y;
-        int idx;
+    static class Node {
+        final int x, y, idx;
+        Node left, right;
         
-        Node(int x, int y, int idx){
+        Node(int x, int y, int idx) {
             this.x = x;
             this.y = y;
             this.idx = idx;
         }
-        int getX(){return x;}
-        int getY(){return y;}
     }
     
-    class Tree{
-        Node root;
-        Tree left;
-        Tree right;
-        
-        Tree(Node root){
-            this.root = root;
-            left = null;
-            right = null;
-        }
-    }
     public int[][] solution(int[][] nodeinfo) {
+        List<Node> nodes = IntStream.range(0, nodeinfo.length)
+            .mapToObj(i -> new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1))
+            .sorted(Comparator.comparing((Node n) -> n.y).reversed()
+                              .thenComparing((Node n) -> n.x))
+            .collect(Collectors.toList());
         
-        List<Node> list = new ArrayList<>();
-        for(int i = 1 ; i <= nodeinfo.length ; i++){
-            list.add(new Node(nodeinfo[i - 1][0], nodeinfo[i - 1][1], i));
+        Node root = buildTree(nodes);
+        
+        List<Integer> preorder = new ArrayList<>();
+        List<Integer> postorder = new ArrayList<>();
+        
+        traverse(root, preorder, postorder);
+        
+        return new int[][]{
+            preorder.stream().mapToInt(Integer::intValue).toArray(),
+            postorder.stream().mapToInt(Integer::intValue).toArray()
+        };
+    }
+    
+    private Node buildTree(List<Node> nodes) {
+        Node root = nodes.get(0);
+        for (int i = 1; i < nodes.size(); i++) {
+            insertNode(root, nodes.get(i));
         }
-        
-        // node 2번쨰 원소 내림차순, 1번째 원소 오름차순
-        list.sort(Comparator.comparing(Node::getY).reversed()
-                            .thenComparing(Comparator.comparing(Node::getX)));
-        
-        // 트리 생성
-        Tree tree = new Tree(list.get(0));
-        
-        for(int i = 1; i < list.size() ; i++){
-            int cur =  list.get(i).getX();
-            Tree tmp = tree;
-            while(true){
-                if(tmp.root.getX() < cur){
-                    if(tmp.right == null) break;
-                    tmp = tmp.right;
-                }else{
-                    if(tmp.left == null) break;
-                    tmp = tmp.left;
-                }
-            }
-            if(tmp.root.getX() < cur) tmp.right = new Tree(list.get(i));
-            else tmp.left = new Tree(list.get(i));
+        return root;
+    }
+    
+    private void insertNode(Node parent, Node newNode) {
+        if (newNode.x < parent.x) {
+            if (parent.left == null) parent.left = newNode;
+            else insertNode(parent.left, newNode);
+        } else {
+            if (parent.right == null) parent.right = newNode;
+            else insertNode(parent.right, newNode);
         }
-        
-        // 트리 순회
-        List<Integer> result1 = new ArrayList<>();
-        printTree(tree, result1);
-        
-        List<Integer> result2 = new ArrayList<>();
-        printTree2(tree, result2);
-        
-        return new int[][]{result1.stream()
-                                  .mapToInt(Integer::valueOf)
-                                  .toArray(),
-                          result2.stream()
-                                 .mapToInt(Integer::valueOf)
-                                 .toArray()};
     }
     
-    void printTree(Tree tree, List<Integer> result) {
-        result.add(tree.root.idx);
-        if(tree.left != null) printTree(tree.left, result);
-        if(tree.right != null) printTree(tree.right, result);
-    }
-    
-    void printTree2(Tree tree, List<Integer> result){
-        if(tree == null) return;
+    private void traverse(Node node, List<Integer> preorder, List<Integer> postorder) {
+        if (node == null) return;
         
-        printTree2(tree.left,result);
-        printTree2(tree.right, result);
-        result.add(tree.root.idx);
+        preorder.add(node.idx);
+        traverse(node.left, preorder, postorder);
+        traverse(node.right, preorder, postorder);
+        postorder.add(node.idx);
     }
-    
 }
