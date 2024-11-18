4c6a8559ed99c1c132b25552b100a9bd4de81af3
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-11-18 17:49:42 +0900

    [level 3] Title: 카운트 다운, Time: 83.22 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,15 +1,15 @@
-import java.util.stream.*;
 import java.util.*;
 
 class Solution {
-    class Node {
-        int cnt;
+    
+    class Node{
+        int c;
         int s;
         int d;
         int t;
         int b;
-        Node(int cnt, int s, int d, int t, int b) {
-            this.cnt = cnt;
+        Node(int c, int s, int d, int t, int b){
+            this.c = c;
             this.s = s;
             this.d = d;
             this.t = t;
@@ -17,73 +17,57 @@ class Solution {
         }
     }
     
-    Node[] dp = new Node[100001];
+    Node[] dp = new Node[100000 + 1];
     
     public int[] solution(int target) {
-        for(int i = 0; i <= target; i++) {
-            dp[i] = new Node(-1, 0, 0, 0, 0);
-        }
-        
-        Node result = solve(target);
-        return new int[]{result.cnt, result.s + result.b};
+        for(int i = 0 ; i < dp.length ; i++) dp[i] = new Node(-1,0,0,0,0);
+        workDp(target);
+        return new int[]{dp[target].c, dp[target].s + dp[target].b};
     }
     
-    Node solve(int n) {
-        if(n < 0) return new Node(-1, 0, 0, 0, 0);
-        if(n == 0) return new Node(0, 0, 0, 0, 0);
+    Node workDp(int target){
         
-        if(dp[n].cnt != -1) return dp[n];
+        if(target < 0) return new Node(-1,0,0,0,0);
+        if(target == 0) return new Node(0,0,0,0,0);
         
-        Node cur = new Node(Integer.MAX_VALUE, 0, 0, 0, 0);
+        if(dp[target].c != -1) return dp[target];
+        
+        Node ret = new Node(Integer.MAX_VALUE, 0,0,0,0);
         
         // 불
-        if(n >= 50) {
-            Node bullResult = solve(n - 50);
-            if(bullResult.cnt != -1) {
-                cur = updateNode(cur, bullResult, 0, 0, 0, 1);
-            }
+        Node n = workDp(target - 50);
+        if(n.c != -1){
+            ret = updateNode(ret, n, 0, 0, 0, 1);
         }
         
         // 싱글
-        for(int i = 1; i <= 20; i++) {
-            Node result = solve(n - i);
-            if(result.cnt != -1) {
-                cur = updateNode(cur, result, 1, 0, 0, 0);
-            }
+        for(int i = 1 ; i <= 20 ;i++){
+            n = workDp(target - i);
+            if(n.c != -1) ret = updateNode(ret, n, 1, 0, 0, 0);
         }
         
         // 더블
-        for(int i = 1; i <= 20; i++) {
-            Node result = solve(n - i * 2);
-            if(result.cnt != -1) {
-                cur = updateNode(cur, result, 0, 1, 0, 0);
-            }
+        for(int i = 1; i <= 20 ; i++){
+            n = workDp(target - i * 2);
+            if(n.c != -1) ret = updateNode(ret, n, 0, 1, 0, 0);
+            
         }
-
+        
         // 트리플
-        for(int i = 1; i <= 20; i++) {
-            Node result = solve(n - i * 3);
-            if(result.cnt != -1) {
-                cur = updateNode(cur, result, 0, 0, 1, 0);
-            }
+        for(int i = 1; i <= 20 ; i++){
+            n = workDp(target - i * 3);
+            if(n.c != -1) ret = updateNode(ret, n, 0, 0, 1, 0);
         }
         
-        if(cur.cnt != Integer.MAX_VALUE) {
-            dp[n] = cur;
-        }
+        if(ret.c != Integer.MAX_VALUE) dp[target] = ret;
+        return dp[target];
         
-        return dp[n];
     }
     
-    private Node updateNode(Node cur, Node result, int s, int d, int t, int b) {
-        int newCnt = result.cnt + 1;
-        if(newCnt < cur.cnt || 
-           (newCnt == cur.cnt && result.s + result.b + s + b > cur.s + cur.b)) {
-            return new Node(newCnt, 
-                          result.s + s, 
-                          result.d + d, 
-                          result.t + t, 
-                          result.b + b);
+    Node updateNode(Node cur, Node result, int s, int d, int t, int b){
+        int cnt = result.c + 1;
+        if((cnt < cur.c) || (cnt == cur.c && cur.b + cur.s < result.b + b + result.s + s)){
+            return new Node(cnt, result.s + s, result.d + d, result.t + t, result.b + b);
         }
         return cur;
     }
