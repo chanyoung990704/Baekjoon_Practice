c499be48cdc2836350156655b8e2fe76843b28ae
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-12-16 14:38:51 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 914.06 ms, Memory: 925 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,48 +1,42 @@
 import java.util.*;
 
 class Solution {
+    private static int[][][] cnt;
     public int solution(int[][] points, int[][] routes) {
-        int maxPossibleTime = 20000; // 최대 좌표가 100이므로, 최대 이동 시간은 200을 넘지 않음
-        int[][][] cnt = new int[maxPossibleTime + 1][100][100];
         int answer = 0;
         int maxTime = 0;
-        
+        cnt = new int[15000][100][100];
+
         for(int[] r : routes){
             int t = 0;
             for(int i = 0 ; i < r.length - 1; i++) {
                 int y = points[r[i] - 1][0] - 1;
                 int x = points[r[i] - 1][1] - 1;
                 
-                if(i == 0 && t < maxPossibleTime) cnt[t][y][x]++;
+                if(i == 0) cnt[t][y][x]++;
 
                 int ny = points[r[i + 1] - 1][0] - 1;
                 int nx = points[r[i + 1] - 1][1] - 1;
                                
                 while(y != ny){
                     t++;
-                    if(t >= maxPossibleTime) break; // 범위 체크 추가
                     if(y < ny) y++;
                     else y--;
                     cnt[t][y][x]++;
                 }
                 
-                if(t >= maxPossibleTime) break;
-                
                 while(x != nx){
                     t++;
-                    if(t >= maxPossibleTime) break; // 범위 체크 추가
                     if(x < nx) x++;
                     else x--;
                     cnt[t][y][x]++;
                 }
-                
-                if(t >= maxPossibleTime) break;
+                    
             }
-            maxTime = Math.max(maxTime, Math.min(t, maxPossibleTime - 1));
+            maxTime = Math.max(maxTime, t);
         }
         
-        // 실제 발생한 maxTime까지만 확인
-        for(int i = 0 ; i <= maxTime ; i++) {
+        for(int i = 0 ; i < cnt.length ; i++) {
             for(int y = 0 ; y < 100 ; y++)
                 for(int x = 0 ; x < 100 ; x++)
                     if(cnt[i][y][x] > 1) answer++;
