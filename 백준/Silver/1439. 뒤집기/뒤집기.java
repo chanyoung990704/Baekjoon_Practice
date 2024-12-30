51e7cf234faa32a905937bd477b4d8d956b486d9
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-12-30 18:16:09 +0900

    [Silver V] Title: 뒤집기, Time: 116 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,29 +1,36 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
+import java.util.regex.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
 
-        String input = br.readLine();
-        int cnt0 = 0; // 0에서 1로 변경되는 횟수
-        int cnt1 = 0; // 1에서 0으로 변경되는 횟수
+        int zero = 0;
+        int one = 0;
 
-        // 첫 번째 문자에 따라 초기 카운트 설정
-        if (input.charAt(0) == '0') cnt1++;
-        else cnt0++;
+        // 0 그룹 찾기
+        Pattern p = Pattern.compile("0+");
+        Matcher m = p.matcher(S);
 
-        // 연속된 숫자가 바뀔 때마다 카운트 증가
-        for (int i = 1; i < input.length(); i++) {
-            if (input.charAt(i) != input.charAt(i-1)) {
-                if (input.charAt(i) == '0') cnt1++;
-                else cnt0++;
-            }
+        while (m.find()) {
+            zero++;
         }
 
-        // 두 카운트 중 최소값 출력
-        System.out.println(Math.min(cnt0, cnt1));
+        // 1그룹 찾기
+        p = Pattern.compile("1+");
+        m = p.matcher(S);
+        while (m.find()) {
+            one++;
+        }
+
+        System.out.println(Math.min(zero, one));
 
         br.close();
+
+
     }
 }
