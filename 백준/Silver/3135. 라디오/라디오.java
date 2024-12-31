e03a497c37e4328ddd8165cd08a8cad079720d0b
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2024-12-31 12:38:01 +0900

    [Silver V] Title: 라디오, Time: 112 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/3135. 라디오/라디오.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] AB = Arrays.stream(br.readLine().split(" "))
+        .mapToInt(Integer::valueOf).toArray();
+
+        int N = Integer.valueOf(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < N ; i++) list.add(Integer.valueOf(br.readLine()));
+
+        // 가장 절댓값이 작은 것을 시작점으로 
+        int cnt = 0;
+        int start = AB[0];
+        int end = AB[1];
+        int dist = Math.abs(start - end);
+
+        boolean isPossible = false;
+
+        for(int cur : list)
+            if(Math.abs(cur - end) < dist){
+                start = cur;
+                dist = Math.abs(cur - end);
+                isPossible = true;
+            }
+
+        if(isPossible) cnt = 1;
+        
+        System.out.println(cnt + dist);
+
+
+
+        br.close();
+
+
+    }
+}
