f78b4f989c674402f4eb98efe91d818ea514d22b
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-04 13:27:34 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 120 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.java
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.java
@@ -1,34 +1,31 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Integer> N = Arrays.stream(br.readLine().split(""))
-        .map(i -> Integer.valueOf(i))
-        .collect(Collectors.toList());
+        List<Integer> list = Arrays.stream(br.readLine().split(""))
+        .map(Integer::valueOf).collect(Collectors.toList());
 
         int lo = 0;
-        int hi = N.size() - 1;
+        int hi = list.size() - 1;
 
-        int left, right;
-        left = right = 0;
-        while(lo < hi){
-            left += N.get(lo);
-            right += N.get(hi);
-            lo++;
-            hi--;
+        int left = 0;
+        int right = 0;
+
+        while (lo < hi) {
+            left += list.get(lo++);
+            right += list.get(hi--);
         }
 
-        String ret = (left == right) ? "LUCKY"
-        : "READY"; 
+        if(left == right) System.out.println("LUCKY");
+        else System.out.println("READY");
 
-        System.out.println(ret);
-        
+
         br.close();
-    }
 
+    }
 }
