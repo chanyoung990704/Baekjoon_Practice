825010c2fe186dcf4f92ac99dfd958f2ffefd38d
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-06 11:07:02 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> AB = Arrays.stream(br.readLine().split(" "))
+        .map(Integer::valueOf).collect(Collectors.toList());
+
+        int A = AB.get(0);
+        int B = AB.get(1);
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
