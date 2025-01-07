42a292175f32f0810d14174d40bc3929bbe93361
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-07 11:45:21 +0900

    [Silver II] Title: A → B, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -6,28 +6,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Long> AB = Arrays.stream(br.readLine().split(" "))
-        .map(Long::valueOf).collect(Collectors.toList());
+        List<Integer> AB = Arrays.stream(br.readLine().split(" "))
+        .map(Integer::valueOf).collect(Collectors.toList());
 
-        long A = AB.get(0);
-        long B = AB.get(1);
+        int A = AB.get(0);
+        int B = AB.get(1);
 
         int cnt = 0;
         while (B >= A) {
             if(B == A) break;
-            
-            if(B % 10 == 1) {
-                B /= 10;
-            } else if(B % 2 == 0) {
-                B /= 2;
-            } else {
-                cnt = -2;  // -1을 출력하기 위한 값
-                break;
-            }
+
+            if(B % 10 == 1) B /= 10;
+            else if(B % 2 == 0) B /= 2;
+            else break;
             cnt++;
         }
 
-        if(A != B) System.out.println(-1);
+        if(A != B) System.out.println(-1); 
         else System.out.println(cnt + 1);
     }
 }
