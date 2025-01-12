b02e81bb955499ec6eba83e3be604f1d021f7b83
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-12 22:59:21 +0900

    [Gold V] Title: 치킨 배달, Time: 176 ms, Memory: 16712 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,61 +1,79 @@
-import java.io.*;
 import java.util.*;
 import java.util.stream.*;
+import java.util.regex.*;
+import java.io.*;
 
 public class Main {
-    static int res = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException{
         
-        int[] NM = Arrays.stream(br.readLine().split(" "))
-        .mapToInt(Integer::valueOf)
-        .toArray();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<List<Integer>> idxs = new ArrayList<>();
-        for(int i = 0 ; i < NM[0] ; i++){
-            List<Integer> list = Arrays.stream(br.readLine().split(" "))
-            .map(cur -> Integer.valueOf(cur))
-            .collect(Collectors.toList());
-            idxs.add(list);
+        List<Integer> NM = Arrays.stream(br.readLine().split(" "))
+        .map(Integer::valueOf).collect(Collectors.toList());
+
+        int N = NM.get(0);
+        int M = NM.get(1);
+
+        List<List<Integer>> city = new ArrayList<>();
+        for(int i = 0 ; i < N ; i++){
+            city.add(Arrays.stream(br.readLine().split(" "))
+            .map(Integer::valueOf).collect(Collectors.toList()));
         }
 
-        List<List<Integer>> chickens = new ArrayList<>();
-        List<List<Integer>> houses = new ArrayList<>();
-        for(int i = 0 ; i < NM[0] ; i++){
-            for(int j = 0 ; j < NM[0] ; j++){
-                if(idxs.get(i).get(j) == 1) houses.add(List.of(i, j));
-                else if(idxs.get(i).get(j) == 2) chickens.add(List.of(i, j));
+        List<int[]> houses = new ArrayList<>();
+        List<int[]> chickens = new ArrayList<>();
+
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                if(city.get(i).get(j) == 1){
+                    houses.add(new int[]{i, j});
+                }
+                if(city.get(i).get(j) == 2){
+                    chickens.add(new int[]{i, j});
+                }
             }
         }
-        combination(houses, chickens, 0, NM[1], new ArrayList<>());
 
-        System.out.println(res);
+        comb(houses, chickens, new ArrayList<>(), 0, M);
 
-        br.close();
+        System.out.println(min);
+
     }
 
-    static void combination(List<List<Integer>> houses, List<List<Integer>> chickens, int idx, int size, List<List<Integer>> result){
-        if(result.size() == size){
-            int curCost = getDist(houses, result);
-            res = Math.min(res, curCost);
-            return;
+    static void comb(List<int[]> houses, List<int[]> chickens, List<int[]> res, int idx, int M){
+        if(res.size() == M){
+            calDistance(houses, res);
         }
 
-        for(int i = idx ; i < chickens.size() ; i++){
-            result.add(chickens.get(i));
-            combination(houses, chickens, i + 1, size, result);
-            result.remove(result.size() - 1);
+        int len = chickens.size();
+        for(int i = idx ; i < len ; i++){
+            int[] cur = chickens.get(i);
+            res.add(cur);
+            comb(houses, chickens, res, i + 1, M);
+            res.remove(res.size() - 1);
         }
     }
 
-    static int getDist(List<List<Integer>> houses, List<List<Integer>> result){
+    static void calDistance(List<int[]> houses, List<int[]> res){
         int total = 0;
-        for(List<Integer> house : houses){
-            int dist = Integer.MAX_VALUE;
-            for(List<Integer> r : result) dist = Math.min(dist, Math.abs(house.get(0) - r.get(0)) + Math.abs(house.get(1) - r.get(1)));
-            total += dist;
+        for(int i = 0 ; i < houses.size() ; i++){
+            int[] house = houses.get(i);
+            int houseDist = Integer.MAX_VALUE;
+            for(int j = 0 ; j < res.size() ; j++){
+                int[] chicken = res.get(j);
+                int dist = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
+                if(dist < houseDist){
+                    houseDist = dist;
+                }
+            }
+            total += houseDist;
         }
-        return total;
+
+        if(total < min){
+            min = total;
+        }
     }
 }
