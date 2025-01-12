4516388b6562376c7e6977cd89017e218468ba5a
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-12 15:27:16 +0900

    [level 3] Title: 기둥과 보 설치, Time: 300.36 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/README.md
+++ 프로그래머스/3/60061. 기둥과 보 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기둥과 보 설치 - 60061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60061?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60061) 
 
 ### 성능 요약
 
-메모리: 115 MB, 시간: 132.69 ms
+메모리: 133 MB, 시간: 300.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 18일 15:13:50
+2025년 01월 12일 15:27:30
 
 ### 문제 설명
 
--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -2,44 +2,142 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    Set<List<Integer>> set = new HashSet<>();
     
-    public int[][] solution(int n, int[][] build_frame) {        
-        for(int[] b : build_frame){
-            int x = b[0], y = b[1], stuff = b[2], op = b[3];
-            List<Integer> structure = List.of(x, y, stuff);
-            
-            if(op == 1){
-                set.add(structure);
-                if(!isPossible()) set.remove(structure);
-            } else if(op == 0){
-                set.remove(structure);
-                if(!isPossible()) set.add(structure);
+    class Node{
+        int y;
+        int x;
+        Node(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
+    }
+    
+    int n;
+    int[][] build_frame;
+    Map<String, Boolean> gi = new HashMap<>(); // 기둥
+    Map<String, Boolean> bo = new HashMap<>(); // 보
+        
+    public int[][] solution(int n, int[][] build_frame) {
+        
+   
+        this.n = n;
+        this.build_frame = build_frame;
+        
+        for(int[] bf : build_frame){
+            int x = bf[0];
+            int y = bf[1];
+            int a = bf[2];
+            int b = bf[3];
+            String key = String.valueOf(x) + " " + String.valueOf(y);
+            // 설치
+            if(b == 1){
+                if(a == 0){
+                    gi.put(key, true);
+                    if(!possibleGi()){
+                        gi.remove(key);
+                    }
+                }else{
+                    bo.put(key, true);
+                    if(!possibleBo()){
+                        bo.remove(key);
+                    }
+                }
+            }
+            else if(b == 0){ // 삭제 시
+                if(a == 0){
+                    gi.remove(key);
+                    if(!possibleGi() || !possibleBo()){ // 둘 다 검사
+                        gi.put(key, true);
+                    }
+                }else{
+                    bo.remove(key);
+                    if(!possibleGi() || !possibleBo()){ // 둘 다 검사
+                        bo.put(key, true);
+                    }
+                }                
             }
         }
         
-        return set.stream()
-            .sorted((a, b) -> {
-                if(!a.get(0).equals(b.get(0))) return a.get(0) - b.get(0);
-                else if(!a.get(1).equals(b.get(1))) return a.get(1) - b.get(1);
-                else return a.get(2) - b.get(2);
-            })
-            .map(l -> new int[]{l.get(0), l.get(1), l.get(2)})
+        List<List<Integer>> res = new ArrayList<>();
+        for(Map.Entry<String, Boolean> e : gi.entrySet()){
+            List<Integer> list = Arrays.stream(e.getKey().split(" "))
+                .map(Integer::valueOf).collect(Collectors.toList());
+            list.add(0);
+            res.add(list);
+        }
+        for(Map.Entry<String, Boolean> e : bo.entrySet()){
+            List<Integer> list = Arrays.stream(e.getKey().split(" "))
+                .map(Integer::valueOf).collect(Collectors.toList());
+            list.add(1);
+            res.add(list);
+        }   
+        
+        res.sort(Comparator.comparing((List<Integer> l) -> l.get(0))
+                .thenComparing((List<Integer> l) -> l.get(1))
+                .thenComparing((List<Integer> l) -> l.get(2)));
+        
+        return res.stream().map(l -> l.stream().mapToInt(Integer::valueOf).toArray())
             .toArray(int[][]::new);
     }
     
-    boolean isPossible(){
-        for(List<Integer> s : set) {
-            int x = s.get(0), y = s.get(1), stuff = s.get(2);
-            if(stuff == 0){ // 기둥
-                if(y == 0 || set.contains(Arrays.asList(x-1, y, 1)) || 
-                   set.contains(Arrays.asList(x, y, 1)) || 
-                   set.contains(Arrays.asList(x, y-1, 0))) continue;
+    boolean possibleGi(){
+        
+        // 기둥 확인
+        for(Map.Entry<String, Boolean> e : gi.entrySet()) {
+            String[] key = e.getKey().split(" ");
+            boolean isPossible = false;
+            // 기둥이 바닥에 있을 때
+            if(key[1].equals("0")){
+                isPossible = true;
+            }
+            
+            // 기둥이 다른 기둥 위에 있을 때
+            int y = Integer.valueOf(key[1]) - 1;
+            String newKey = key[0] + " " + String.valueOf(y);
+            if(gi.containsKey(newKey)){
+                isPossible = true;
+            }
+            
+            // 보의 한쪽 끝 부분 위에 있는 경우
+            int x = Integer.valueOf(key[0]) - 1;
+            String newBoKey = String.valueOf(x) + " " + key[1];
+            if(bo.containsKey(newBoKey) || bo.containsKey(e.getKey())){
+                isPossible = true;
+            }
+            
+            if(!isPossible){
                 return false;
-            } else if(stuff == 1){ // 보
-                if(set.contains(Arrays.asList(x, y-1, 0)) || 
-                   set.contains(Arrays.asList(x+1, y-1, 0)) ||
-                   (set.contains(Arrays.asList(x-1, y, 1)) && set.contains(Arrays.asList(x+1, y, 1)))) continue;
+            }
+        }
+        
+        return true;
+    }
+    
+    boolean possibleBo(){
+        for(Map.Entry<String, Boolean> e : bo.entrySet()) {
+            String[] key = e.getKey().split(" ");
+            boolean isPossible = false;
+            
+            // 한 쪽 끝 부분이 기둥 위
+            int y = Integer.valueOf(key[1]);
+            int x = Integer.valueOf(key[0]);
+            
+            String newKeyFirst = String.valueOf(x) + " " + String.valueOf(y - 1);
+            String newKeySecond = String.valueOf(x + 1) + " " + String.valueOf(y - 1);
+            
+            if(gi.containsKey(newKeyFirst) || gi.containsKey(newKeySecond)){
+                isPossible = true;
+            }
+            
+            // 양 쪽 끝 부분이 다른 보와 동시에 연결
+            String newkeyFirsts = String.valueOf(x - 1) + " " + String.valueOf(y);
+            String newKeySeconds = String.valueOf(x + 1) + " " + String.valueOf(y);
+            
+            if(bo.containsKey(newkeyFirsts) && bo.containsKey(newKeySeconds)){
+                isPossible = true;
+            }
+            
+            if(!isPossible){
                 return false;
             }
         }
