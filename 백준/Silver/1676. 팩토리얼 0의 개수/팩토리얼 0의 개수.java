f7c988df27b53976fa3e5fba006228ebd268eaca
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-13 22:14:10 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.valueOf(br.readLine());
+
+        int cnt = 0;
+
+        for(int i = 5 ; i <= N ; i *= 5){
+            cnt += N / i;
+        }
+
+        System.out.println(cnt);
+    }
+}
