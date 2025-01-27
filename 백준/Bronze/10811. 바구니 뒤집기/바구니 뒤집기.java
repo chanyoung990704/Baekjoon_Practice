54f3ea19cc0815354eb63230c7f2104cba326e39
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-01-27 22:22:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+
+        List<Integer> NM = Arrays.stream(br.readLine().split(" "))
+        .map(Integer::valueOf).collect(Collectors.toList());
+
+        int N = NM.get(0);
+        int M = NM.get(1);
+
+        List<Integer> list = IntStream.range(1, N + 1)
+        .mapToObj(Integer::valueOf).collect(Collectors.toList());
+
+        for(int i = 0 ; i < M ; i++){
+            List<Integer> cur = Arrays.stream(br.readLine().split(" "))
+            .map(Integer::valueOf).collect(Collectors.toList());
+            
+            int j = cur.get(0) - 1;
+            int k = cur.get(1) - 1;
+
+            while (j < k) {
+                swap(list, j, k);
+                j++;
+                k--;
+            }
+        }
+
+        System.out.println(list.stream()
+        .map(i -> String.valueOf(i)).collect(Collectors.joining(" ")));
+    }
+
+    static void swap(List<Integer> list, int i, int j){
+        int v = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, v);
+    }
+}
