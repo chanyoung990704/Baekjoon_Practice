b0eb5d68773c0013badc6334915e91085462d0df
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-02-05 11:08:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,29 @@
-import java.io.*;
 import java.util.*;
 import java.util.stream.*;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.regex.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
 
         int N = Integer.valueOf(br.readLine());
 
-        int[] dp = new int[5001];
-        int max = 1000000000;
-        Arrays.fill(dp, max);
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, 100000000);
         dp[0] = 0;
 
-        for(int i = 1 ; i <= N ; i++){
-            if(i >= 3) dp[i] = Math.min(dp[i], dp[i - 3] + 1);
-            if(i >= 5) dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+        for(int i = 3; i<= N ; i++){
+            if(dp[i - 3] != 100000000){
+                dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            }
+            if(i >= 5 && dp[i - 5] != 100000000){
+                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+            }
         }
 
-        if(dp[N] == max) System.out.println(-1);
-        else System.out.println(dp[N]);
-
-        br.close();
-
-
+        System.out.println(dp[N] == 100000000 ? -1 : dp[N]);
     }
 }
+
