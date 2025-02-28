d3a1df1c97c698d857bf6f97e6126201cb2bced6
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-02-28 12:12:28 +0900

    [level 1] Title: 체육복, Time: 2.42 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 0.18 ms
+메모리: 83.7 MB, 시간: 2.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 14일 17:49:51
+2025년 02월 28일 12:13:06
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,39 +1,33 @@
 import java.util.*;
-
+import java.util.stream.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Map<Integer, Integer> map = new HashMap<>();
         
-        // 모든 학생이 체육복을 1개씩 가지고 있다고 초기화
-        for(int i = 1 ; i <= n ; i++)
-            map.put(i, 1);
+        int[] students = new int[n + 2];
+        Arrays.fill(students, 1);
         
-        // 여벌 체육복을 가진 학생 처리
-        for(int r : reserve)
-            map.put(r, map.get(r) + 1);
+        for(int l : lost){
+            students[l]--;
+        }
+        for(int r : reserve){
+            students[r]++;
+        }
         
-        // 체육복을 도난당한 학생 처리
-        for(int l : lost)
-            map.put(l, map.get(l) - 1);
-        
-        // 체육수업을 들을 수 있는 학생 수 계산
-        int answer = 0;
-        
-        for(int i = 1 ; i <= n ; i++){
-            if(map.get(i) >= 1) {
-                answer++;
-                continue;
-            }
-            
-            if(i > 1 && map.get(i-1) > 1) {
-                map.put(i-1, map.get(i-1) - 1);
-                answer++;
-            } else if(i < n && map.get(i+1) > 1) {
-                map.put(i+1, map.get(i+1) - 1);
-                answer++;
+        // 1번학생부터 확인
+        for(int i = 1; i <= n ; i++){
+            if(students[i] == 0){
+                if(students[i - 1] == 2){
+                    students[i - 1]--;
+                    students[i]++;
+                }
+                else if(students[i + 1] == 2){
+                    students[i + 1]--;
+                    students[i]++;
+                }
             }
         }
         
-        return answer;
+        return (int)IntStream.range(1, n + 1)
+            .filter(i -> students[i] >= 1).count();
     }
 }
