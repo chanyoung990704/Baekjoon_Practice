7113b509cddbcb92c29b7be49a4b6a9f59469caa
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-07 21:47:22 +0900

    [Silver II] Title: 한 줄로 서기, Time: 116 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,53 +1,34 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.*;
 
 public class Main {
-
-    static List<Integer> ans = new ArrayList<>();
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.valueOf(br.readLine());
 
-        List<Integer> list = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::valueOf).collect(Collectors.toList());
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] room = new int[N];
 
-        permutation(list, new boolean[11], new ArrayList<>());
-
-        System.out.println(
-            ans.stream().map(String::valueOf).collect(Collectors.joining(" "))
-        );
-    }
-
-
-    static void permutation(List<Integer> list, boolean[] visited, List<Integer> res){
-
-        if(res.size() == list.size()){
-            int[] cnt = new int[list.size()];
-            for(int i = 0 ; i < res.size() ; i++){
-                int cur = res.get(i);
-                int bigMan = 0;
-                for(int j = i - 1 ; j >= 0 ; j--)
-                    if(res.get(j) > cur) bigMan++;
-                cnt[cur - 1] = bigMan;
-            }
-
-            for(int i = 0 ; i < list.size() ; i++){
-                if(list.get(i) != cnt[i]) return;
-            }
-            ans = new ArrayList<>(res);
-            return;
-        }
-
-        for(int i = 1 ; i <= list.size() ; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                res.add(i);
-                permutation(list, visited, res);
-                res.remove(res.size() - 1);
-                visited[i] = false;
+        for(int i = 0; i < N; i++) {
+            int cnt = nums[i];
+            // 빈 방 찾기
+            for(int j = 0 ; j < N ; j++){
+                if(room[j] == 0 && cnt == 0){
+                    room[j] = i + 1;
+                    break;
+                }else if(room[j] == 0){
+                    cnt--;
+                }
             }
         }
+
+        System.out.println(Arrays.stream(room)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" ")));
+
     }
 }
