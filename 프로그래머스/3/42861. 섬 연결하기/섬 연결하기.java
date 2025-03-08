62e5f58403305cceaac45b4e2761ddb4e43cdf62
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-08 11:24:26 +0900

    [level 3] Title: 섬 연결하기, Time: 1.30 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -3,36 +3,44 @@ import java.util.stream.*;
 
 class Solution {
     
-    int[] parent;
+    Map<Integer, Integer> parent = new HashMap<>();
     
-    public int solution(int n, int[][] costs) {
-        int answer = 0;
-        
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-        parent = new int[n];
-        for(int i = 0 ; i < n ; i++) parent[i] = i;
-        
-        for(int[] cost : costs){
-            // 사이클이 아닌 경우 추가
-            if(findParent(cost[0]) != findParent(cost[1])) {
-                unionParent(cost[0], cost[1]);
-                answer += cost[2];
-            }
+    int findParent(int n){
+        if(!parent.containsKey(n)){
+            parent.put(n, n);
+        }else if(parent.get(n) != n){
+            parent.put(n, findParent(parent.get(n)));
         }
-        
-        return answer;
-    }
-    
-    int findParent(int a) {
-        if(parent[a] != a) parent[a] = findParent(parent[a]);
-        return parent[a];
+        return parent.get(n);
     }
     
     void unionParent(int a, int b){
-        int rootA = findParent(a);
-        int rootB = findParent(b);
+        if(findParent(a) < findParent(b)){
+            parent.put(findParent(b), findParent(a));
+        }else{
+            parent.put(findParent(a), findParent(b));
+        }
+    }
+    
+    public int solution(int n, int[][] costs) {
         
-        if(rootA < rootB) parent[rootB] = rootA;
-        else parent[rootA] = rootB;
+        // 최소 간선 정렬
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        int answer = 0;
+        
+        for(int i = 0 ; i < costs.length ; i++){
+            int t = costs[i][0];
+            int f = costs[i][1];
+            
+            // 사이클이면 건너뛰기
+            if(findParent(t) == findParent(f)){
+                continue;
+            }
+            
+            answer += costs[i][2];
+            unionParent(t, f);
+        }
+        
+        return answer;
     }
 }
