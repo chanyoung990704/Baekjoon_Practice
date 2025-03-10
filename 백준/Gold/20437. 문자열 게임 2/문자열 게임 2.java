d0bb2b327f3de34c21e371007e53ff07dc6f5183
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-10 10:51:25 +0900

    [Gold V] Title: 문자열 게임 2, Time: 408 ms, Memory: 38292 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,76 +1,49 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.*;
-import java.io.*;
-import java.time.LocalDate;
-import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.valueOf(br.readLine());
 
         while (T-- > 0) {
-
             String W = br.readLine();
-            Map<Character, List<Integer>> map = new HashMap<>();
-            for(int i = 0 ; i < W.length() ; i++){
-                char c = W.charAt(i);
-                map.computeIfAbsent(c, (k) -> new ArrayList<>()).add(i);
-            }
-    
             int K = Integer.valueOf(br.readLine());
-    
-            int[] cnt = new int[26];
-    
-            int lo = 0;
-            int hi = 0;
-            int min = Integer.MAX_VALUE;
-    
-            while (hi < W.length()) {
-                char cur = W.charAt(hi);
-                cnt[cur - 'a']++;
-    
-                while (Arrays.stream(cnt).anyMatch(i -> i == K)) {
-                    min = Math.min(min, hi - lo + 1);
-                    char prev = W.charAt(lo);
-                    cnt[prev - 'a']--;
-                    lo++;
-                }
-                hi++;
+
+            Map<Character, List<Integer>> map = new HashMap<>();
+
+            for(int i = 0; i < W.length(); i++) {
+                char c = W.charAt(i);
+                map.computeIfAbsent(c, k -> new ArrayList<>()).add(i);
             }
-    
-            // 큰 수
+
+            // 어떤 문자를 정확히 K개 포함 && 짧은 문자열
+            int min = Integer.MAX_VALUE;
             int max = Integer.MIN_VALUE;
-            for(Map.Entry<Character, List<Integer>> e : map.entrySet()){
-                List<Integer> v = e.getValue();
-                if(v.size() < K){
+            for(Map.Entry<Character, List<Integer>> entry : map.entrySet()) {
+                List<Integer> value = entry.getValue();
+                if (value.size() < K) {
                     continue;
                 }
-
-                int i = 0;
-
-                while (i + K <= v.size()) {
-                    int s = v.get(i);
-                    int end = v.get(i + K - 1);
-                    max = Math.max(max, end - s + 1);
-                    i++;
+                for(int i = 0 ; i + K <= value.size(); i++) {
+                    min = Math.min(min, value.get(i + K - 1) - value.get(i) + 1);
+                    max = Math.max(max, value.get(i + K - 1) - value.get(i) + 1);
                 }
-    
             }
-    
-    
-    
-            if(max == Integer.MIN_VALUE || min == Integer.MAX_VALUE){
+
+            if(min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) {
                 System.out.println(-1);
-            }else{
-                System.out.println(min + " " + max);
+                continue;
             }
-            
+            System.out.println(min + " " + max);
+
         }
+
     }
 }
-
-
-
