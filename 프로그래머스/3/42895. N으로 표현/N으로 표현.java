4c5511faf917ffef49acf2b75069c677f5c52960
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-10 21:26:25 +0900

    [level 3] Title: N으로 표현, Time: 429.76 ms, Memory: 295 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,28 +1,38 @@
-import java.util.stream.*;
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int N, int number) {
         
-        List<Set<Integer>> dpList = new ArrayList<>();
-        for(int i = 0 ; i <= 8 ; i++) dpList.add(new HashSet<>());
-        
-        String strN = String.valueOf(N);
-        for(int i = 1 ; i <= 8 ; i++) dpList.get(i).add(Integer.parseInt(strN.repeat(i)));
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 1 ; i <= 8 ; i++){
+            String nStr = String.valueOf(N).repeat(i);
+            map.computeIfAbsent(i, k -> new ArrayList<>()).add(Integer.valueOf(nStr));
+        }
         
         for(int i = 1 ; i <= 8 ; i++){
-            for(int j = 1 ; j < i ; j++)
-                for(int j_num : dpList.get(j))
-                    for(int opposite_num : dpList.get(i - j)){
-                        Set<Integer> curSet = dpList.get(i);
-                        curSet.add(j_num + opposite_num);
-                        curSet.add(j_num - opposite_num);
-                        curSet.add(j_num * opposite_num);
-                        if(opposite_num != 0) curSet.add(j_num / opposite_num);
-                    }                 
+            List<Integer> l = map.get(i);
             
-            if(dpList.get(i).contains(number)) return i;
-        }  
+            for(int a = 1; a < i ; a++){
+                int b = i - a;
+                // 각각 구하기
+                for(int na : map.get(a)){
+                    for(int nb : map.get(b)){
+                        l.add(na + nb);
+                        l.add(na - nb);
+                        l.add(na * nb);
+                        if(nb != 0){
+                            l.add(na / nb);
+                        }
+                    }
+                }
+            }
+            
+            if(l.contains(number)){
+                return i;
+            }
+        }
+        
         return -1;
     }
 }
