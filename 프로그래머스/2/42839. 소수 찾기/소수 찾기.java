2d78ab4ec1567ce27092826fe5dbfcd6574af165
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-10 15:35:11 +0900

    [level 2] Title: 소수 찾기, Time: 160.92 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,63 @@
+import java.util.stream.*;
 import java.util.*;
 
 class Solution {
-    
-    boolean[] visited;
-    List<Integer> list;
-    
     public int solution(String numbers) {
         int answer = 0;
+        int[] nums = Arrays.stream(numbers.split(""))
+            .mapToInt(Integer::valueOf).toArray();
         
-        visited = new boolean[numbers.length()];
-        list = new ArrayList<>();
-        bruteForce(numbers, "");
+        Set<Integer> set = new HashSet<>();
+        solve(nums, "", new boolean[nums.length], set);
         
-        int max = list.stream().mapToInt(Integer::valueOf).max().orElse(0);
-        if(max < 2){
-            return 0;
-        }
-        
-        boolean[] isPrime = new boolean[max + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-        
-        for(int i = 2 ; i <= Math.sqrt(max) ; i++)
-            if(isPrime[i])
-                for(int j = i * i ; j <= max ; j += i)
-                    isPrime[j] = false;
-        
-        return (int)list.stream()
-            .distinct()
-            .filter(i -> isPrime[i])
-            .count();
-        
-    }
-    
-    void bruteForce(String numbers, String result) {
-        if(result.length() > 0){
-            list.add(Integer.valueOf(result));
-        }
-        
-        for(int i = 0 ; i < numbers.length() ; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                bruteForce(numbers, result + numbers.charAt(i));
-                visited[i] = false;
+        // 소수 판정
+        for(int n : set){
+            if(n < 2){
+                continue;
+            }
+            
+            boolean isPrime = true;
+            for(int i = 2 ; i < n ; i++){
+                if(n % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if(isPrime){
+                answer++;
             }
         }
+        
+        return answer;
     }
     
+    
+    void solve(int[] nums, String res, boolean[] visited, Set<Integer> set){
+        
+        if(res.length() > 0){
+            set.add(Integer.valueOf(res));
+        }
+        
+        // 처음 숫자는 0이면 안됨
+        for(int i = 0 ; i < nums.length ; i++){
+            int n = nums[i];
+            if(res.length() == 0 && n == 0){
+                continue;
+            }
+            
+            // 백트래킹
+            if(!visited[i]){
+                StringBuilder sb = new StringBuilder(res);
+                int len = res.length();
+                sb.append(n);
+                visited[i] = true;
+                solve(nums, sb.toString(), visited, set);
+                visited[i] = false;
+                sb.setLength(len);
+            }
+            
+        }
+        
+    }
 }
