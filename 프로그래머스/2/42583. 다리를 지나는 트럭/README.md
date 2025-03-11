be8e9d2e98eb89fe2da50c2311e17913f71215c1
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-11 11:21:09 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 72.36 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 32.67 ms
+메모리: 78.4 MB, 시간: 72.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 10:56:21
+2025년 03월 11일 11:21:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,49 +1,43 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    
-    class Truck{
-        int weight;
-        int start;
-        
-        Truck(int weight, int start){
-            this.weight = weight;
-            this.start = start;
-        }
-    }
-    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
+        
+        Deque<Integer> running = new ArrayDeque<>();
+        Deque<Integer> trucks = new ArrayDeque<>(Arrays.stream(truck_weights).boxed()
+                                                 .collect(Collectors.toList()));
+        for(int i = 0 ; i < bridge_length ; i++){
+            running.offer(0);
+        }
         
         int time = 0;
         int curWeight = 0;
-        int idx = 0;
-        Deque<Truck> q = new ArrayDeque<>();
+        int curSize = 0;
         
-        while(true){
+        while(!running.isEmpty() || !trucks.isEmpty()){
+            
+            // 러닝 큐를 한칸씩 이동
+            if(!running.isEmpty()){
+                curWeight -= running.pollFirst();
+                curSize -= 1;
+            }
+            
+            // 무게 & 용량 확인 후 푸시
+            if(!trucks.isEmpty()){
+                int next = trucks.peekFirst();
+                if(curWeight + next <= weight && curSize + 1 <= bridge_length){
+                    curWeight += next;
+                    curSize += 1;
+                    running.offerLast(next);
+                    trucks.pollFirst();
+                }else{
+                    running.offerLast(0);
+                }
+            }
+            
             time++;
-            
-            // 빠져나갈 트럭 확인
-            if(!q.isEmpty() && q.peekFirst().start + bridge_length == time){
-                curWeight -= q.pollFirst().weight;
-            }
-            
-            // 들어올 수 있는 트럭 확인
-            if(idx < truck_weights.length && 
-                 curWeight + truck_weights[idx] <= weight){
-                q.offerLast(new Truck(truck_weights[idx], time));
-                curWeight += truck_weights[idx];
-                idx++;
-            }
-            
-            if(q.isEmpty()){
-                break;
-            }
-
         }
-        
-        
-        
         return time;
     }
 }
