65bafaa1121d65a938bfc66a8da4c6a7f0415a17
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-11 11:35:09 +0900

    [level 1] Title: K번째수, Time: 9.67 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,18 +3,14 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for(int[] command : commands){    
-            List<Integer> cur = Arrays
-                .stream(Arrays.copyOfRange(array, command[0] - 1, command[1]))
-                .sorted()
-                .boxed()
-                .collect(Collectors.toList());
-            answer.add(cur.get(command[2] - 1));
+        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+        List<Integer> ret = new ArrayList<>();
+        for(int[] c : commands){
+            List<Integer> sub = new ArrayList<>(list.subList(c[0] - 1, c[1]));
+            Collections.sort(sub, Comparator.naturalOrder());
+            ret.add(sub.get(c[2] - 1));
         }
         
-        return answer.stream().mapToInt(Integer::valueOf)
-            .toArray();
+        return ret.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 84.2 MB, 시간: 19.83 ms
+메모리: 81 MB, 시간: 9.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 30일 14:38:19
+2025년 03월 11일 11:35:51
 
 ### 문제 설명
 
