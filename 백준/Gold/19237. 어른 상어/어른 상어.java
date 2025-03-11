13dc9cf2c729bcf47e8a1616cee0488c2a840a81
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-11 15:47:13 +0900

    [Gold II] Title: 어른 상어, Time: 200 ms, Memory: 27324 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.java
+++ 백준/Gold/19237. 어른 상어/어른 상어.java
@@ -1,189 +1,204 @@
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.*;
 
 public class Main {
 
-    static class Node{
-        int idx;
-        int dir;
-        int t; // limit time
+    static int N, M, k;
 
-        Node(int idx, int dir, int t){
-            this.idx = idx;
-            this.dir = dir;
-            this.t = t;
-        }
-    }
+    static int[][][] smells; // y, x, 0 -> 상어 번호 , y, x, 1 -> 냄새
+    static int[][] sharks; // 상어번호 0 -> y좌표, 상어번호 1 -> x 좌표 상어번호 2 -> 방향
+    static int[][][] priorities; // 상어 번호 방향 우선순위
+    static boolean[] isAlive; // 상어 생존 여부
 
-    static class NextShark{
-        Node node;
-        int y;
-        int x;
-        NextShark(Node node, int y, int x){
-            this.node = node;
-            this.y = y;
-            this.x = x;
-        }
-    }
+    static int[] dy = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    static int[] dx = {0, 0, -1, 1};
 
-    // not use 0 idx
-    static int[] dy = new int[]{-2, -1,1,0,0};
-    static int[] dx = new int[]{-2, 0,0,-1,1};
-
-    static List<List<Node>> adj = new ArrayList<>();
-    static List<List<List<Integer>>> sharkDirs = new ArrayList<>();
-    static int time = 0;
-    static int k = -1;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] NMk = brToArr(br);
+
+        int[] NMk = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = NMk[0];
+        M = NMk[1];
         k = NMk[2];
 
-        Map<Integer, List<Integer>> sharkMap = new HashMap<>();
-        Map<Integer, Integer> sharkDirMap = new HashMap<>();
-        for(int i = 0 ; i < NMk[0] ; i++){
-            int[] jArr = brToArr(br);
-            adj.add(new ArrayList<>());
-            for(int j = 0 ; j < NMk[0] ; j++){
-                if(jArr[j] > 0) sharkMap.put(jArr[j], List.of(i, j));
-                adj.get(i).add(new Node(0, 0, 0));
+        smells = new int[N][N][2];
+        sharks = new int[M + 1][3];
+        priorities = new int[M + 1][4][4];
+        isAlive = new boolean[M + 1];
+
+        for (int i = 0; i < N; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < N; j++) {
+                if (input[j] > 0) {
+                    // 상어 입력
+                    sharks[input[j]][0] = i;
+                    sharks[input[j]][1] = j;
+                    isAlive[input[j]] = true;
+                    // 냄새
+                    smells[i][j][0] = input[j];
+                    smells[i][j][1] = k;
+                }
             }
         }
 
-        int[] dirArr = brToArr(br);
-        for(int i = 0 ; i < dirArr.length ; i++) sharkDirMap.put(i + 1, dirArr[i]);
-
-        for(int i = 1 ; i <= NMk[1] ; i++){
-            List<Integer> dir = sharkMap.get(i);
-            int y = dir.get(0);
-            int x = dir.get(1);
-            adj.get(y).set(x, new Node(i, sharkDirMap.get(i), k));
+        // 방향 입력
+        int[] dirs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+                .map(i -> i - 1).toArray();
+        for (int i = 1; i <= M; i++) {
+            sharks[i][2] = dirs[i - 1];
         }
 
-        for(int i = 0 ; i <= NMk[1] ; i++) sharkDirs.add(new ArrayList<>());
-        for(int i = 1 ; i <= NMk[1] ; i++){
-            sharkDirs.get(i).add(new ArrayList<>());
-            for(int j = 0 ; j < 4 ; j++){
-                sharkDirs.get(i).add(Arrays.stream(br.readLine().split(" "))
-                                                     .map(Integer::valueOf)
-                                                     .collect(Collectors.toList()));
+        // 우선순위 입력
+        for (int i = 1; i <= M; i++) {
+            for (int j = 0; j < 4; j++) {
+                priorities[i][j] = Arrays.stream(br.readLine().split(" "))
+                        .mapToInt(Integer::parseInt).map(n -> n - 1).toArray();
             }
         }
 
-        // start Logic
-        while (true) {
-            // basecase
-            if(isOnlyOne()) break;
-            if(time >= 1000) {
-                time = -1;
-                break;
+        // 시뮬레이션
+        int time = 0;
+        while (time <= 1000) {
+            // 1번 상어만 남았는지 확인
+            if (onlyFirst()) {
+                System.out.println(time);
+                return;
             }
 
-            moveShark();
+            // 모든 상어 이동
+            allSharkMove();
+
+            // 냄새 감소
+            decreaseSmell();
+
+            // 새 냄새
+            addSmell();
+
             time++;
+
         }
 
-        // print time
-        System.out.println(time);
-        br.close();
+        System.out.println(-1);
+
     }
 
-    static void moveShark() {
-        List<List<Integer>> sharkIdx = getSharkIdx();
-        int N = adj.size();
-        
-        // 임시 맵 생성 및 초기화
-        List<List<Node>> tempMap = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            tempMap.add(new ArrayList<>());
-            for(int j = 0; j < N; j++) {
-                Node currentNode = adj.get(i).get(j);
-                // 기존 냄새 정보 복사 (시간은 1 감소)
-                if(currentNode.t > 0) {
-                    tempMap.get(i).add(new Node(currentNode.idx, currentNode.dir, currentNode.t - 1));
-                } else {
-                    tempMap.get(i).add(new Node(0, 0, 0));
-                }
+    private static void addSmell() {
+        for (int i = 1; i <= M; i++) {
+            if (isAlive[i]) {
+                int y = sharks[i][0];
+                int x = sharks[i][1];
+                smells[y][x][0] = i;
+                smells[y][x][1] = k;
             }
         }
+    }
 
-        for(List<Integer> shark : sharkIdx){
-            Node cur = adj.get(shark.get(0)).get(shark.get(1));
-            List<Integer> priority = sharkDirs.get(cur.idx).get(cur.dir);
-            boolean isMoved = false;
-            
-            // 냄새가 없는 칸으로 이동 시도
-            for(int d : priority){
-                int ny = shark.get(0) + dy[d];
-                int nx = shark.get(1) + dx[d];
-                if(isBoundary(ny, nx) && adj.get(ny).get(nx).t == 0){
-                    updateTempMap(tempMap, ny, nx, new Node(cur.idx, d, k));
-                    isMoved = true;
-                    break;
-                }
-            }
-            
-            // 자신의 냄새가 있는 칸으로 이동
-            if(!isMoved){
-                for(int d : priority){
-                    int ny = shark.get(0) + dy[d];
-                    int nx = shark.get(1) + dx[d];
-                    boolean isFound = false;
-                    if(isBoundary(ny, nx) && adj.get(ny).get(nx).idx == cur.idx){
-                        updateTempMap(tempMap, ny, nx, new Node(cur.idx, d, k));
-                        isFound = true;
-                        break;
+    private static void decreaseSmell() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (smells[i][j][1] > 0) {
+                    smells[i][j][1]--;
+                    if (smells[i][j][1] == 0) {
+                        smells[i][j][0] = 0;
                     }
-                    if(isFound) break;
                 }
             }
         }
+    }
 
-        // 임시 맵을 실제 맵으로 복사
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                adj.get(i).set(j, tempMap.get(i).get(j));
+    private static void allSharkMove() {
+
+        int[][] nextPos = new int[M + 1][3];
+        int[][] sharkMap = new int[N][N];
+
+        for (int i = 1; i <= M; i++) {
+            // 죽었으면
+            if(!isAlive[i]){
+                continue;
+            }
+
+            int y = sharks[i][0];
+            int x = sharks[i][1];
+            int dir = sharks[i][2];
+
+            boolean foundEmpty = false;
+            boolean foundOwnSmell = false;
+            int[] emptySpace = new int[3];
+            int[] ownSmellSpace = new int[3];
+
+            // 우선순위 대로 탐색
+            for(int d : priorities[i][dir]){
+                int ny = y + dy[d];
+                int nx = x + dx[d];
+
+                if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
+                    // 빈칸이면
+                    if (smells[ny][nx][1] == 0) {
+                        emptySpace[0] = ny;
+                        emptySpace[1] = nx;
+                        emptySpace[2] = d;
+                        foundEmpty = true;
+                        break;
+                    } else if (!foundOwnSmell && smells[ny][nx][0] == i) {
+                        ownSmellSpace[0] = ny;
+                        ownSmellSpace[1] = nx;
+                        ownSmellSpace[2] = d;
+                        foundOwnSmell = true;
+
+                    }
+                }
+            }
+
+            // 이동
+            if(foundEmpty){
+                nextPos[i][0] = emptySpace[0];
+                nextPos[i][1] = emptySpace[1];
+                nextPos[i][2] = emptySpace[2];
+            }
+            else if(foundOwnSmell){
+                nextPos[i][0] = ownSmellSpace[0];
+                nextPos[i][1] = ownSmellSpace[1];
+                nextPos[i][2] = ownSmellSpace[2];
+            }
+        }
+
+        // 충돌 처리
+        for(int i = 1 ; i <= M; i++) {
+            if(!isAlive[i]){
+                continue;
+            }
+
+            int ny = nextPos[i][0];
+            int nx = nextPos[i][1];
+
+            if (sharkMap[ny][nx] == 0) {
+                sharkMap[ny][nx] = i;
+                sharks[i][0] = ny;
+                sharks[i][1] = nx;
+                sharks[i][2] = nextPos[i][2];
+            }else{
+                isAlive[i] = false;
             }
         }
     }
 
-    // 임시 맵 업데이트 메소드
-    static void updateTempMap(List<List<Node>> tempMap, int y, int x, Node newNode) {
-        Node existing = tempMap.get(y).get(x);
-        if(existing.t == k) { 
-            if(newNode.idx < existing.idx) { 
-                tempMap.get(y).set(x, newNode);
-            }
-        } else {
-            tempMap.get(y).set(x, newNode);
+    private static boolean onlyFirst() {
+        // 1번은 살아있는지 확인
+        if (!isAlive[1]) {
+            return false;
         }
-    }
 
-    static boolean isBoundary(int y, int x){
-        return y >= 0 && y < adj.size() && x >= 0 && x < adj.size();
-    }
+        for (int i = 2; i <= M; i++) {
+            if(isAlive[i]){
+                return false;
+            }
+        }
 
-    static List<List<Integer>> getSharkIdx() {
-        List<List<Integer>> ret = new ArrayList<>();
-        for(int i = 0 ; i < adj.size() ; i++)
-            for(int j = 0 ; j < adj.size() ; j++)
-                if(adj.get(i).get(j).t == k) ret.add(List.of(i, j));
-        return ret;
-    }
-
-    static int[] brToArr(BufferedReader br) throws IOException{
-        return Arrays.stream(br.readLine().split(" "))
-                     .mapToInt(Integer::parseInt)
-                     .toArray();
-    }
-
-    static boolean isOnlyOne(){
-        boolean ret = true;
-        for(List<Node> l : adj)
-            for(Node n : l)
-                if(n.idx != 0 && n.idx != 1 && n.t == k) return false;
-        return ret;
+        return true;
     }
 }
