c5b90c2a80454a46ec332213e33fccac9f501d5e
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-12 12:26:32 +0900

    [Gold V] Title: 두 용액, Time: 340 ms, Memory: 29452 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,67 +1,45 @@
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.*;
 
-public class Main {    
+public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.valueOf(br.readLine());
 
-        int lo = -1;
-        int hi = -1;
-        int val = Integer.MAX_VALUE;
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(nums);
 
-        List<Integer> list = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::valueOf).sorted().collect(Collectors.toList());
-
-        for(int i = 0 ; i < list.size() ; i++) {
-            int cur = list.get(i);
-            int target = -1 * cur;
-
-            int idx = getIdx(list, target);
-
-            if(idx < list.size() && idx != i){
-                int v = Math.abs(list.get(i) + list.get(idx));
-                if(v < val){
-                    lo = Math.min(i, idx);
-                    hi = Math.max(i, idx);
-                    val = v;
-                }
-            }
-
-            if(idx - 1 >= 0 && idx - 1 != i) {
-                int v = Math.abs(list.get(i) + list.get(idx - 1));
-                if(v < val){
-                    lo = Math.min(i, idx - 1);
-                    hi = Math.max(i, idx - 1);
-                    val = v;
-                }
-            }
-        }
-
-
-        System.out.println(list.get(lo) + " " + list.get(hi));
-    }
-
-    // 이분탐색
-    static int getIdx(List<Integer> list, int target){
         int lo = 0;
-        int hi = list.size() - 1;
-        int ret = list.size() - 1;
+        int hi = nums.length - 1;
 
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            int cur = list.get(mid);
+        int rlo = 0;
+        int rhi = nums.length - 1;
 
-            if(cur >= target) {
-                ret = mid;
-                hi = mid - 1;
+        while (lo < hi) {
+            int total = nums[lo] + nums[hi];
+            int prev = nums[rlo] + nums[rhi];
+
+            if(Math.abs(total - 0) < Math.abs(prev - 0)) {
+                rlo = lo;
+                rhi = hi;
+            }
+
+            if(total > 0){
+                hi--;
+            } else if (total < 0) {
+                lo++;
             }else{
-                lo = mid + 1;
+                break;
             }
         }
-        
-        return ret;
+
+        System.out.println(nums[rlo] + " " + nums[rhi]);
     }
 }
