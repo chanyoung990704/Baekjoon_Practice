ca9c4a53187c304543496fc7f36fafd567ce1287
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-16 12:14:00 +0900

    [Gold IV] Title: 전화번호 목록, Time: 484 ms, Memory: 35472 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,34 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.*;
-import java.io.*;
-import java.time.LocalDate;
-import java.util.regex.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.valueOf(br.readLine());
-        while (t -- > 0) {
+
+        while (t-- > 0) {
+
             int n = Integer.valueOf(br.readLine());
+
             List<String> list = new ArrayList<>();
-            for(int i = 0 ; i < n;  i++){
+            for(int i = 0; i < n; i++) {
                 list.add(br.readLine());
             }
 
             list.sort(Comparator.naturalOrder());
-            boolean consist = false;
 
-            for(int i = 0 ; i < n - 1;  i++){
-                if(list.get(i + 1).startsWith(list.get(i))){
-                    consist = true;
+            int i = 0;
+            for (; i < list.size() - 1; i++) {
+                if (list.get(i + 1).startsWith(list.get(i))) {
+                    System.out.println("NO");
                     break;
                 }
             }
 
-            System.out.println(consist == true ? "NO" : "YES");
+            if(i == list.size() - 1) {
+                System.out.println("YES");
+            }
+
         }
     }
 }
-
-
