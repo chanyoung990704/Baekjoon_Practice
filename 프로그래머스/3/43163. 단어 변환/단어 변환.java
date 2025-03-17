db93083b34a363d352ac7b90791cdc86644a4624
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-17 14:55:14 +0900

    [level 3] Title: 단어 변환, Time: 0.32 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,36 +2,58 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = isPossible(begin, target, words, new boolean[words.length], 0);
         
-        return answer == 1000000000 ? 0 : answer;
-    }
-    
-    int isPossible(String cur, String target, String[] words, boolean[] visited, int cnt){
-        
-        if(cur.equals(target)){
-            return cnt;
+        // target이 없는 경우
+        boolean noTarget = true;
+        for(String word : words){
+            if(word.equals(target)){
+                noTarget = false;
+                break;
+            }
+        }
+        if(noTarget){
+            return 0;
         }
         
-        int min = 1000000000;
+        // BFS진행
+        Deque<String> dq = new ArrayDeque<>();
+        Map<String, Integer> map = new HashMap<>();
         
-        // 다음 단어 가능한지
-        for(int i = 0 ; i < words.length ; i++){
-            String word = words[i];
-            int diff = 0;
-            for(int j = 0 ; j < word.length() ; j++){
-                if(cur.charAt(j) != word.charAt(j)){
-                    diff++;
+        dq.offer(begin);
+        map.put(begin, 0);
+        
+        while(!dq.isEmpty()){
+            String cur = dq.pollFirst();
+            int cnt = map.get(cur);
+            
+            if(cur.equals(target)){
+                return cnt;
+            }
+            
+            // 다음 단어 변환 가능한지
+            for(String word : words){
+                if(!map.containsKey(word) && isPossible(cur, word)){
+                    dq.offerLast(word);
+                    map.put(word, cnt + 1);
                 }
             }
-            // 한글자만 차이나야 함
-            if(diff == 1 && !visited[i]){
-                visited[i] = true;
-                min = Math.min(min, isPossible(word, target, words, visited, cnt + 1));
-                visited[i] = false;
+        }
+        
+        return 0;
+    }
+    
+    boolean isPossible(String cur, String word){
+        if(cur.length() != word.length()){
+            return false;
+        }
+        int diff = 0;
+        for(int i = 0 ; i < cur.length() ; i++){
+            if(cur.charAt(i) != word.charAt(i)){
+                diff++;
             }
         }
         
-        return min;
+        return diff == 1;
     }
+
 }
