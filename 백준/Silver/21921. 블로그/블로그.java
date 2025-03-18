bc5abd371f7ea166996241bf9a04f54c8b2273bb
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-18 14:02:47 +0900

    [Silver III] Title: 블로그, Time: 328 ms, Memory: 39348 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,38 +1,52 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.*;
 
+
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
+        // 지난 일수 N 기간 X
         int[] NX = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int N = NX[0];
         int X = NX[1];
 
-        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        // N일 접속량
+        int[] users = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        // 투포인터
         int lo = 0;
         int hi = 0;
-        int cnt = 0;
-        int sum = 0;
-        int max = 0;
 
-        while (hi < nums.length) {
-            sum += nums[hi];
-            while (hi - lo + 1 >= X) {
-                if (sum == max) {
-                    cnt++;
-                }else if(sum > max){
-                    cnt = 1;
-                    max = sum;
-                }
-                sum -= nums[lo];
+        int sum = 0;
+        int max = -1;
+        int cnt = 0;
+
+        while (hi < users.length) {
+            sum += users[hi];
+
+            // X일 초과 제외
+            while (hi - lo + 1 > X){
+                sum -= users[lo];
                 lo++;
             }
+
+            // 최대 갱신
+            if (sum > max) {
+                max = sum;
+                cnt = 1;
+            } else if (sum == max) {
+                cnt++;
+            }
+
             hi++;
         }
 
@@ -43,5 +57,7 @@ public class Main {
 
         System.out.println(max);
         System.out.println(cnt);
+
     }
+
 }
