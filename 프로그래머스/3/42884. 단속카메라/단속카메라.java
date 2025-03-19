51f87b2e313d1c8c7781a65737a366d8c0d6439f
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-19 13:07:24 +0900

    [level 3] Title: 단속카메라, Time: 10.75 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,20 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] routes) {
-        
+        // 도착시간 정렬
         Arrays.sort(routes, (a, b) -> a[1] - b[1]);
         
-        int target = routes[0][1];
-        int answer = 1;
+        // 겹치는 구간 찾기
+        int cnt = 0;
+        int start = 0;
+        int end = -50000;
         
-        for(int i = 1; i < routes.length ; i++) {
-            int start = routes[i][0];
-            if(start > target){
-                answer++;
-                target = routes[i][1];
+        for(int i = 0 ; i < routes.length ; i++){
+            int[] cur = routes[i];
+            // 새로운 구간의 시작이면
+            if(end < cur[0]){
+                // 구간 개수 
+                cnt++;
+                // 초기화
+                start = cur[0];
+                end = cur[1];
             }
         }
         
-        return answer;
+        return cnt;
     }
 }
