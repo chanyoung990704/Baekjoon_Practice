09f11a8416c3c8d427c5557f0b59e2847ed2d066
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-19 14:22:42 +0900

    [level 3] Title: 기지국 설치, Time: 2.82 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,22 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int coverage = 2 * w + 1;
+
+        // 구간 만들기
+        List<Integer> d = new ArrayList<>();
         int start = 1;
-        
-        for (int s : stations) {
-            if (s - w > start) {
-                int count = (s - w) - start;
-                answer += (count + coverage - 1) / coverage;
+        for(int station : stations){
+            // 전파 최소 지점
+            int min = station - w;
+            // 시작 지점이 최소 지점보다 뒤에 있어야 구간 형성
+            if(start < min){
+                // 구간의 개수 저장
+                d.add(min - 1 - start + 1);
             }
-            start = s + w + 1;
+            start = station + w + 1;
         }
         
-        if (start <= n) {
-            int count = n - start + 1;
-            answer += (count + coverage - 1) / coverage;
+        // 마지막 구간이 있는 경우
+        if(start <= n){
+            d.add(n - start + 1);
         }
         
-        return answer;
+        // 개당 커버리지
+        int coverage = 2 * w + 1;
+        int cnt = 0;
+        
+        for(int num : d){
+            cnt += (num + coverage - 1) / coverage;
+        }
+
+        return cnt;
     }
 }
