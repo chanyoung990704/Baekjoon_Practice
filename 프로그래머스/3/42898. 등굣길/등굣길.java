54c674e910699eaa9545db9338b878e7d1504a79
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-19 12:56:34 +0900

    [level 3] Title: 등굣길, Time: 2.69 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,28 +1,53 @@
+import java.util.stream.*;
 import java.util.*;
-
 class Solution {
-    int[][] arr;
-    int[][] dist;
-    int n;
-    int m;
-    public int solution(int m, int n, int[][] puddles) {
-        this.n = n;
-        this.m = m;
-        arr = new int[n][m];
-        dist = new int[n][m];
+    public int solution(int m, int n, int[][] puddles) {        
+        // dp
+        int[][] dp = new int[n][m];
+        for(int i = 0 ; i < n ; i++){
+            Arrays.fill(dp[i], -1);
+        }
         
-        for(int[] puddle : puddles) arr[puddle[1] - 1][puddle[0] - 1] = 1;
-        for(int i = 0 ; i < n ; i++) Arrays.fill(dist[i], -1);
+        // 맵
+        int[][] arr = new int[n][m];
+        // 웅덩이 -1
+        for(int[] idx : puddles){
+            arr[idx[1] - 1][idx[0] - 1] = -1;
+        }
         
-        return solve(0, 0);
+        // 시작 지점부터 탐색
+        return minDist(arr, dp, 0, 0) ;
     }
     
-    int solve(int y, int x){
-        if(y >= n || x >= m || arr[y][x] == 1) return 0; // 범위를 벗어나거나 웅덩이인 경우
-        if(y == n - 1 && x == m - 1) return 1; // 목적지에 도달한 경우
-        if(dist[y][x] != -1) return dist[y][x];
-        
-        dist[y][x] = (solve(y + 1, x) + solve(y, x + 1)) % 1000000007;
-        return dist[y][x];
+    int minDist(int[][] arr, int[][] dp, int y, int x){
+        // 도착했으면
+        if(y == arr.length - 1 && x == arr[0].length - 1){
+            return 1;
+        }
+
+        // dp에 저장된 값이 있으면 반환
+        if(dp[y][x] != -1){
+            return dp[y][x];
+        }
+
+        // 방향 오른쪽, 아래
+        int[] dy = new int[]{0, 1};
+        int[] dx = new int[]{1, 0};
+
+        // 경로 개수 초기화
+        dp[y][x] = 0;
+
+        for(int i = 0; i < 2; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(ny >= 0 && ny < arr.length && nx >= 0 && nx < arr[0].length){
+                if(arr[ny][nx] != -1){
+                    dp[y][x] = (dp[y][x] + minDist(arr, dp, ny, nx)) % 1000000007;
+                }
+            }
+        }
+
+        return dp[y][x];
     }
 }
