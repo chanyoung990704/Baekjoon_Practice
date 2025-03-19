80eeceb3a1dc966ca93c0305537496b637ae5044
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-19 12:26:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 33.45 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,28 +1,40 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // 트리맵
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         
-        for (String op : operations) {
-            String[] parts = op.split(" ");
-            String command = parts[0];
-            int num = Integer.parseInt(parts[1]);
-            
-            if (command.equals("I")) {
-                maxHeap.offer(num);
-                minHeap.offer(num);
-            } else if (!maxHeap.isEmpty()) {
-                if (num == 1) {
-                    minHeap.remove(maxHeap.poll());
-                } else {
-                    maxHeap.remove(minHeap.poll());
+        // 삽입
+        for(String op : operations){
+            // 숫자 파싱
+            int num = Integer.valueOf(op.substring(op.lastIndexOf(" ") + 1));
+            // 삽입
+            if(op.startsWith("I")){
+                map.put(num, 1);
+            }
+            // 삭제
+            else if(op.startsWith("D")){
+                if(map.isEmpty()){
+                    continue;
+                }
+                
+                if(num == 1){
+                    // 최댓값 삭제
+                    map.remove(map.lastKey());
+                }else if(num == -1){
+                    // 최소값 삭제
+                    map.remove(map.firstKey());
                 }
             }
         }
-        
-        if (maxHeap.isEmpty()) return new int[]{0, 0};
-        return new int[]{maxHeap.peek(), minHeap.peek()};
+        int[] answer = new int[2];
+        if(map.isEmpty()){
+            return new int[]{0,0};
+        }
+        answer[0] = map.lastKey();
+        answer[1] = map.firstKey();
+        return answer;
     }
 }
