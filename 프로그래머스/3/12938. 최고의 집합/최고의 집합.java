040cc17e6a8cfd0d514b39fa704968ee3bef38f0
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-19 13:57:39 +0900

    [level 3] Title: 최고의 집합, Time: 0.30 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,21 +1,35 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int n, int s) {
-        // n이 s보다 크면 불가능한 경우
-        if (n > s) return new int[]{-1};
         
-        int[] answer = new int[n];
-        int base = s / n;  // 기본 값
-        int remainder = s % n;  // 남은 값
+        // 안되는 경우
+        if(s < n){
+            return new int[]{-1};
+        }
         
-        // 배열 채우기
-        for (int i = 0; i < n; i++) {
-            answer[i] = base;
-            // 남은 값이 있으면 뒤에서부터 1씩 더해줌
-            if (i >= n - remainder) {
-                answer[i]++;
+        int[] res = new int[n];
+        
+        // 몫
+        for(int i = 0 ; i < n ; i++){
+            res[i] = s / n;
+        }
+        
+        // 나머지
+        int remain = s % n;
+        
+        // 뒤에서부터 1씩 증가
+        int idx = n - 1;
+        while(remain > 0){
+            res[idx]++;
+            remain--;
+            idx--;
+            if(idx < 0){
+                idx = n - 1;
             }
         }
         
-        return answer;
+        return res;
     }
 }
