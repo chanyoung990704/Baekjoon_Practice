33e05a39ebba2bcb08da7f643a74d7722790f50a
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-03-19 23:43:13 +0900

    [level 2] Title: 구명보트, Time: 11.89 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,31 +1,42 @@
-import java.util.stream.*;
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        // 사람 몸무게 people
+        // 구명보트 무게제한 limit
         
+        // 정렬
         Arrays.sort(people);
         
+        // 투포인터
         int lo = 0;
         int hi = people.length - 1;
-        
         int answer = 0;
         
         while(lo <= hi){
+            
+            // 겹치는 경우
             if(lo == hi){
                 answer++;
-                return answer;
+                break;
             }
             
+            // 합계
             int sum = people[lo] + people[hi];
-            // 탑승 불가능
+            
+            // 합계가 초과하면 한명만 탈출
             if(sum > limit){
                 hi--;
-            }else{
+            }
+            // 2명 가능
+            else{
                 lo++;
                 hi--;
             }
+            // 개수 증가
             answer++;
+            
         }
         
         return answer;
