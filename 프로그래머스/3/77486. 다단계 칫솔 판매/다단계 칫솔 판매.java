6b5d1330cdcd7cf2426afa8485d9da3b8366d864
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-04-03 12:14:50 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 23.16 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,38 +2,78 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
+    
+    class Node{
+        String name;
+        int price;
+        Node parent = null;
+        List<Node> children = new ArrayList<>();
+        
+        Node(String name){
+            this.name = name;
+        }
+    }
+    
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         
-        Map<String, Integer> cost = new HashMap<>();
-        for(int i = 0 ; i < enroll.length ; i++) cost.put(enroll[i], 0);
-        cost.put("-", 0);
-        Map<String, String> parent = new HashMap<>();
-        for(int i = 0 ; i < enroll.length ; i++) {
-            String child = enroll[i];
-            String parentName = referral[i];
-            parent.put(child, parentName);
-        }
+        // 루트
+        Node root = new Node("center");
+        Map<String, Node> map = new HashMap<>();
+        map.put("-", root);
         
-        
-        for(int i = 0 ; i < seller.length ; i++) {
-            int price = amount[i] * 100;
-            String name = seller[i];
-            cost.put(name, cost.get(name) + price);
-            int remain = price;
-            while(true){
-                if(name.equals("-")) break;
-                if(remain / 10 == 0) break;
-                
-                remain /= 10;
-                // 자신의 것에서 10% 빼고 부모에게 배분한다.
-                cost.put(name, cost.get(name) - remain);
-                name = parent.get(name);
-                cost.put(name, cost.get(name) + remain);
+        // 트리 생성
+        for(int i = 0 ; i < enroll.length ; i++){
+            String e = enroll[i];
+            String r = referral[i];
+            
+            // 맵에 없으면 등록
+            if(!map.containsKey(e)){
+                map.put(e, new Node(e));
             }
+            if(!map.containsKey(r)){
+                map.put(r, new Node(r));
+            }
+            
+            // 노드 가져와서 연결
+            Node parent = map.get(r);
+            Node child = map.get(e);
+            
+            parent.children.add(child);
+            child.parent = parent;
+            
         }
         
-        return Arrays.stream(enroll)
-            .mapToInt(i -> Integer.valueOf(cost.get(i)))
-            .toArray();
+        // seller - amount 계산
+        for(int i = 0 ; i < seller.length ; i++){
+            String s = seller[i];
+            int a = amount[i] * 100;
+            
+            Node start = map.get(s);
+            
+            // 부모가 null일때까지 진행
+            while(start.parent != null){
+                // 금액 계산
+                if(a / 10 < 1){
+                    start.price += a;
+                    break;
+                }
+                // 자신에게 90%
+                start.price += a - (a / 10);
+                a /= 10;
+                
+                // 다음 부모로 이동
+                start = start.parent;
+            }
+            
+        }
+        
+        int[] answer = new int[enroll.length];
+        
+        for(int i = 0 ; i < enroll.length ; i++){
+            answer[i] = map.get(enroll[i]).price;
+        }
+        
+
+        return answer;
     }
 }
