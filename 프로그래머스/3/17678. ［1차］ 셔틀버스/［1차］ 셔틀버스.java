aba9ab999cef1d096b477f78d4d6270027de7eb5
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-04-03 13:12:34 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 6.03 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,68 +1,55 @@
 import java.util.*;
 import java.util.stream.*;
+import java.time.*;
 
 class Solution {
-    int n, t, m;
-    String[] timetable;
     public String solution(int n, int t, int m, String[] timetable) {
-        this.n = n;
-        this.t = t;
-        this.m = m;
-        this.timetable = timetable;
-        String answer = "";
-
         
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(String time : timetable) minHeap.offer(convertTime(time));
+        // 줄 서는게 우선 그리고 탑승
         
-        int curTime = convertTime("9:00");
-        while(true) {
-            // 버스가 한번일 때
-            if(n == 1){
-                // 자리가 충분히 남았을 때
-                if(minHeap.size() < m) answer = convertTimeString(curTime);
-                else {
-                    int lastTime = 0;
-                    for (int i = 0; i < m; i++) {
-                        lastTime = minHeap.poll();
-                    }
-                    answer = convertTimeString(Math.min(curTime, lastTime - 1));
+        Arrays.sort(timetable);
+        
+        int idx = 0; // 타임 테이블
+        LocalTime bus = LocalTime.of(9, 0);
+        LocalTime last = LocalTime.of(0,0);
+        
+        for(int i = 0 ; i < n; i++){
+            int cap = 0;
+            
+            while(cap < m && idx < timetable.length){
+                
+                String[] parse = timetable[idx].split(":");
+                LocalTime cur = LocalTime.of(Integer.valueOf(parse[0]), Integer.valueOf(parse[1]));
+                // 못태우면 종료
+                if(cur.isAfter(bus)){
+                    break;
                 }
-                break;
+                
+                last = cur;
+                idx++;
+                cap++;
+                
             }
             
-            int cnt = 0;
-            // 반복해서 빼주기
-            while(!minHeap.isEmpty() && n > 1 && minHeap.peek() <= curTime && cnt < m){
-                minHeap.poll();
-                cnt++;
+            // 마지막 시간인 경우
+            if(i == n - 1){
+                // 인원 충분한 경우
+                if(cap < m){
+                    return bus.toString();
+                }
+                else{
+                    // 마지막 인덱스보다 1분 더 빠르게
+                    last = last.minusMinutes(1);
+                    return last.toString();
+                }
             }
             
-            curTime += t;
-            n--;
-            
+            // 다음 버스 도착 시간
+            bus = bus.plusMinutes(t);
         }
         
+        
+        String answer = "";
         return answer;
     }
-    
-    int convertTime(String s) {
-        List<Integer> list = Arrays.stream(s.split(":"))
-                                   .map(Integer::valueOf)
-                                   .collect(Collectors.toList());
-        return list.get(0) * 60 + list.get(1);
-    }
-    
-    String convertTimeString(int t) {
-        String res = "";
-        int h = t / 60;
-        if(h < 10) res += "0" + h + ":";
-        else res += h + ":";
-        int m = t % 60;
-        if(m < 10) res += "0" + m;
-        else res += "" + m;
-        
-        return res;
-    }
-    
 }
