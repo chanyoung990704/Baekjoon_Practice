cab40e4d5c79e4b799ecdf2e1a7936d806bdafb7
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2025-04-07 14:54:25 +0900

    [level 3] Title: 표 편집, Time: 375.68 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,43 +1,87 @@
 import java.util.*;
 
 class Solution {
+    
+    class Node {
+        Node next;
+        Node prev;
+        boolean removed;
+    }
+    
     public String solution(int n, int k, String[] cmd) {
         
-        TreeSet<Integer> numArr = new TreeSet<>();
-        Deque<Integer> removed = new ArrayDeque<>();
+        Node start = new Node();
         
-        for(int i = 0 ; i < n ; i++) numArr.add(i);
-        int cur = k;
+        Node[] nodes = new Node[n];
+        nodes[0] = start;
         
-        for(String c : cmd) {
-            String[] ops = c.split(" ");
-            String op = ops[0];
+        // 노드 연결
+        for (int i = 1; i < n; i++) {
+            nodes[i] = new Node();
+            nodes[i - 1].next = nodes[i];
+            nodes[i].prev = nodes[i - 1];
+        }
+        
+        Node cur = nodes[k];
+        Deque<Node> dq = new ArrayDeque<>();
+        
+        for (String c : cmd) {
+            String[] parsed = c.split(" ");
+            String op = parsed[0];
             
-            if(op.equals("D")){
-                int cnt = Integer.parseInt(ops[1]);
-                while(cnt > 0){
-                    cur = numArr.higher(cur);
-                    cnt--;
+            if (op.equals("U") || op.equals("D")) {
+                int m = Integer.valueOf(parsed[1]);
+                for (int i = 0; i < m; i++) {
+                    if (op.equals("U")) {
+                        cur = cur.prev;
+                    } else {
+                        cur = cur.next;
+                    }
                 }
-            }else if(op.equals("U")){
-                int cnt = Integer.parseInt(ops[1]);
-                while(cnt > 0){
-                    cur = numArr.lower(cur);
-                    cnt--; 
+            }
+            
+            // 삭제
+            if (op.equals("C")) {
+                cur.removed = true;
+                dq.offerLast(cur);
+                // 마지막 행인 경우
+                if (cur.next == null) {
+                    cur.prev.next = null;
+                    cur = cur.prev;
+                } else if (cur.prev == null) { // 첫 번째 행인 경우
+                    cur.next.prev = null;
+                    cur = cur.next;
+                } else { // 중간 행인 경우
+                    cur.prev.next = cur.next;
+                    cur.next.prev = cur.prev;
+                    cur = cur.next;
                 }
-            }else if(op.equals("C")){
-                removed.offerLast(cur);
-                numArr.remove(cur);
-                if(numArr.higher(cur) == null) cur = numArr.lower(cur);
-                else cur = numArr.higher(cur);      
-            }else if(op.equals("Z")){
-                numArr.add(removed.pollLast());
+            }
+            
+            // 복구
+            if (op.equals("Z")) {
+                Node restoredNode = dq.pollLast();
+                restoredNode.removed = false;
+                
+                if (restoredNode.prev != null) {
+                    restoredNode.prev.next = restoredNode;
+                }
+                if (restoredNode.next != null) {
+                    restoredNode.next.prev = restoredNode;
+                }
             }
         }
         
+        // 결과 생성
+        StringBuilder answerBuilder = new StringBuilder();
+        for (Node node : nodes) {
+            if (node.removed) {
+                answerBuilder.append("X");
+            } else {
+                answerBuilder.append("O");
+            }
+        }
         
-        StringBuilder sb = new StringBuilder("O".repeat(n));
-        for(int r : removed) sb.setCharAt(r, 'X');
-        return sb.toString();
+        return answerBuilder.toString();
     }
 }
