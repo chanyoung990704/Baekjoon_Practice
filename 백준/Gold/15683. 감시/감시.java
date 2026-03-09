d7680b7f64fabba1f29f0212b4370e5627148844
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-03-09 13:35:10 +0900

    [Gold III] Title: 감시, Time: 276 ms, Memory: 89060 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -2,125 +2,118 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
 
-    static int min = Integer.MAX_VALUE;
+    static int N,M;
+    static int[][] map;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int min = 987654321;
+    static List<CCTV> cctvs = new ArrayList<>();
 
-        int[] NM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int N = NM[0];
-        int M = NM[1];
-
-        int[][] map = new int[N][M];
-
-        for(int i = 0; i < N; i++) {
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    static class CCTV {
+        int y, x, type;
+        CCTV(int y, int x, int type) {
+            this.y = y;
+            this.x = x;
+            this.type = type;
         }
+    }
 
-        // 동 서 남 북
-        int[] dy = new int[] {0, 0, 1, -1};
-        int[] dx = new int[] {1, -1, 0, 0};
-        int[][][] move = new int[][][]{
-                // 1번
-                {
-                        {0},{1},{2},{3}
-                },
-                // 2번
-                {
-                        {0,1},{2,3}
-                },
-                // 3번
-                {
-                        {3,0},{0,2},{2,1},{1,3}
-                },
-                // 4번
-                {
-                        {3,0,1},{0,2,3},{2,0,1},{1,2,3}
-                },
-                // 5번
-                {
-                        {0,1,2,3}
-                }
-        };
+    // 상 우 하 좌
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
 
-        List<int[]> idxs = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(map[i][j] >= 1 && map[i][j] <= 5) { // 수정: 범위 체크 추가
-                    idxs.add(new int[]{i, j});
+    // dirs[type] = 가능한 방향 조합들
+    static int[][][] dirs = {
+            {},
+            {{0}, {1}, {2}, {3}},
+            {{0, 2}, {1, 3}},
+            {{0, 1}, {1, 2}, {2, 3}, {3, 0}},
+            {{0, 1, 3}, {0, 1, 2}, {1, 2, 3}, {0, 2, 3}},
+            {{0, 1, 2, 3}}
+    };
+
+
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            map[i] = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] >= 1 && map[i][j] <= 5) {
+                    cctvs.add(new CCTV(i, j, map[i][j]));
                 }
             }
         }
 
-        bruteForce(0, move, idxs, dy, dx, map);
+        dfs(0, map);
 
         System.out.println(min);
+
     }
 
-    private static void bruteForce(int i, int[][][] move, List<int[]> idxs, int[] dy, int[] dx, int[][] map) {
-        if (i == idxs.size()) {
-            int cnt = 0;
-            for(int y = 0; y < map.length; y++) {
-                for(int x = 0; x < map[0].length; x++) {
-                    if(map[y][x] == 0) {
-                        cnt++;
-                    }
-                }
-            }
-            min = Math.min(min, cnt);
+    private static void dfs(int idx, int[][] map) {
+        // basecase
+        if (idx == cctvs.size()) {
+            min = Math.min(min, countBlind(map));
             return;
         }
 
-        int y = idxs.get(i)[0];
-        int x = idxs.get(i)[1];
-        int val = map[y][x] - 1;
+        CCTV cur = cctvs.get(idx);
 
-        // 수정: 배열 범위 체크 추가
-        if(val < 0 || val >= move.length) {
-            return;
-        }
-
-        for(int[] m : move[val]){
-            int[][] backup = copyArr(map);
-
-            for(int j = 0; j < m.length; j++) {
-                int ny = y;
-                int nx = x;
-                while (true) {
-                    ny += dy[m[j]];
-                    nx += dx[m[j]];
-
-                    if (!isBoundary(map, ny, nx) || map[ny][nx] == 6) {
-                        break;
-                    }
-
-                    if(map[ny][nx] == 0) { // 수정: 빈 공간만 감시 처리
-                        map[ny][nx] = 7;
-                    }
-                }
+        for (int[] dirSet : dirs[cur.type]) {
+            int[][] copied = copyMap(map);
+            for (int dir : dirSet) {
+                watch(cur.y, cur.x, dir, copied);
             }
-
-            bruteForce(i+1, move, idxs, dy, dx, map);
-
-            // 복원
-            for(int r = 0; r < map.length; r++) {
-                for(int c = 0; c < map[0].length; c++) {
-                    map[r][c] = backup[r][c];
-                }
-            }
+            dfs(idx+1, copied);
         }
     }
 
-    private static int[][] copyArr(int[][] map) {
-        int[][] copied = new int[map.length][map[0].length];
-        for(int i = 0; i < map.length; i++) {
-            copied[i] = Arrays.copyOf(map[i], map[i].length);
+    private static void watch(int y, int x, int dir, int[][] copied) {
+        int ny = y, nx = x;
+
+        while (true) {
+            ny += dy[dir];
+            nx += dx[dir];
+
+            if (!isRange(ny, nx) || copied[ny][nx] == 6) {
+                break;
+            }
+
+            if (copied[ny][nx] == 0) {
+                copied[ny][nx] = -1;
+            }
+
+        }
+    }
+
+    private static int countBlind(int[][] map) {
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    private static int[][] copyMap(int[][] map) {
+        int[][] copied = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            copied[i] = Arrays.copyOf(map[i], M);
         }
         return copied;
     }
 
-    private static boolean isBoundary(int[][] map, int ny, int nx) {
-        return ny >= 0 && ny < map.length && nx >= 0 && nx < map[0].length;
+    static boolean isRange(int y, int x) {
+        return 0 <= y && y < N && 0 <= x && x < M;
     }
 }
