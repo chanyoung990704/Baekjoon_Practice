87d9b22a634db25c53184f6f6d16583c4a3da668
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-03-10 15:38:45 +0900

    [Gold V] Title: 치킨 배달, Time: 216 ms, Memory: 16912 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,79 +1,78 @@
-import java.util.*;
-import java.util.stream.*;
-import java.util.regex.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
 
-    static int min = Integer.MAX_VALUE;
+    static int N,M;
+    static int[][] city;
 
-    public static void main(String[] args) throws IOException{
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static List<int[]> chickens = new ArrayList<>(); // 치킨집 위치
+    static List<int[]> houses = new ArrayList<>();
 
-        List<Integer> NM = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::valueOf).collect(Collectors.toList());
+    static int min = 987654321;
 
-        int N = NM.get(0);
-        int M = NM.get(1);
+    public static void main(String[] args) throws IOException {
 
-        List<List<Integer>> city = new ArrayList<>();
-        for(int i = 0 ; i < N ; i++){
-            city.add(Arrays.stream(br.readLine().split(" "))
-            .map(Integer::valueOf).collect(Collectors.toList()));
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        List<int[]> houses = new ArrayList<>();
-        List<int[]> chickens = new ArrayList<>();
-
-        for(int i = 0 ; i < N ; i++){
-            for(int j = 0 ; j < N ; j++){
-                if(city.get(i).get(j) == 1){
-                    houses.add(new int[]{i, j});
-                }
-                if(city.get(i).get(j) == 2){
+        city = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            city[i] = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < N; j++) {
+                if(city[i][j] == 2){
                     chickens.add(new int[]{i, j});
+                }
+                if (city[i][j] == 1) {
+                    houses.add(new int[]{i, j});
                 }
             }
         }
 
-        comb(houses, chickens, new ArrayList<>(), 0, M);
+        dfs(0, 0);
 
         System.out.println(min);
 
     }
 
-    static void comb(List<int[]> houses, List<int[]> chickens, List<int[]> res, int idx, int M){
-        if(res.size() == M){
-            calDistance(houses, res);
+    private static void dfs(int start, int status) {
+        // 최대 M개
+        if (Integer.bitCount(status) == M) {
+            calculate(status);
+            return;
         }
 
-        int len = chickens.size();
-        for(int i = idx ; i < len ; i++){
-            int[] cur = chickens.get(i);
-            res.add(cur);
-            comb(houses, chickens, res, i + 1, M);
-            res.remove(res.size() - 1);
+        // 상태 갱신
+        for (int i = start; i < chickens.size(); i++) {
+            // 현재포함 된것은
+            if((status >> i & 1) == 1){
+                continue;
+            }
+            dfs(i+1,status | (1 << i));
         }
     }
 
-    static void calDistance(List<int[]> houses, List<int[]> res){
+    private static void calculate(int status) {
         int total = 0;
-        for(int i = 0 ; i < houses.size() ; i++){
-            int[] house = houses.get(i);
-            int houseDist = Integer.MAX_VALUE;
-            for(int j = 0 ; j < res.size() ; j++){
-                int[] chicken = res.get(j);
-                int dist = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
-                if(dist < houseDist){
-                    houseDist = dist;
+        // 집마다 반복
+        for (int[] house : houses) {
+            int cur = 987654321;
+            int y = house[0], x = house[1];
+            for (int i = 0; i < chickens.size(); i++) {
+                if ((status >> i & 1) == 1) {
+                    // 거리 계산
+                    int[] chicken = chickens.get(i);
+                    cur = Math.min(cur, Math.abs(chicken[0] - y) + Math.abs(chicken[1] - x));
                 }
             }
-            total += houseDist;
+            // 결과 갱신
+            total += cur;
         }
 
-        if(total < min){
-            min = total;
-        }
+        min = Math.min(min, total);
     }
+
 }
