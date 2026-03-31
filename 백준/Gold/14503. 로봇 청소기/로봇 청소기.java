8bec6dc759aacba29e9886e09de0ae2b5c0b7cd7
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-03-31 10:42:57 +0900

    [Gold V] Title: 로봇 청소기, Time: 116 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,97 +1,94 @@
-import java.util.*;
-import java.util.stream.*;
 import java.io.*;
-import java.time.LocalDate;
-import java.util.regex.*;
+import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+
+    static int[] dy = new int[]{-1, 0, 1, 0};
+    static int[] dx = new int[]{0, 1, 0, -1}; // 북, 동, 남, 서
+
+    static int y, x, dir;
+    static int[][] arr;
+    static boolean[][] clean;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        List<Integer> NM = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::valueOf).collect(Collectors.toList());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int N = NM.get(0);
-        int M = NM.get(1);
+        arr = new int[N][M];
+        clean = new boolean[N][M];
 
-        List<Integer> rcd = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::valueOf).collect(Collectors.toList());
+        st = new StringTokenizer(br.readLine());
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        dir = Integer.parseInt(st.nextToken());
 
-        int r = rcd.get(0);
-        int c = rcd.get(1);
-        int d = rcd.get(2);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-        List<List<Integer>> room = new ArrayList<>();
-        for(int i = 0 ; i < N ; i++){
-            room.add(Arrays.stream(br.readLine().split(" "))
-            .map(Integer::valueOf).collect(Collectors.toList()));
+        while (true) {
+            // 현재 칸 청소
+            clean[y][x] = true;
+
+            boolean blocked = true;
+            int nextDir = dir;
+            // 주변 4칸 반시계 탐색
+            for (int d = 0; d < 4; d++) {
+                nextDir = (nextDir + 3) % 4;
+                // 청소되지 않은 빈 칸인 경우 한칸 전진
+                int ny = y + dy[nextDir];
+                int nx = x + dx[nextDir];
+
+                if (range(ny, nx) && !clean[ny][nx] && arr[ny][nx] == 0) {
+                    blocked = false;
+                    y = ny;
+                    x = nx;
+                    dir = nextDir;
+                    break;
+
+                }
+            }
+
+            // 청소되지 않은 빈칸이 없는 경우
+            if (blocked) {
+                // 바라보는 방향 유지한 채로 한칸 후진
+                int ny = y - dy[dir];
+                int nx = x - dx[dir];
+
+                // 후진 가능하면
+                if (range(ny, nx) && arr[ny][nx] != 1) {
+                    y = ny;
+                    x = nx;
+                }else{
+                    break;
+                }
+            }
         }
 
         int cnt = 0;
-
-        while (true) {
-            // 현재 칸을 본다
-            int cur = room.get(r).get(c);
-            if(cur == 0){
-                // 청소
-                cnt++;
-                room.get(r).set(c, 2); // 2번은 청소가 완료된 칸
-            }
-
-            // 주변 4칸 확인
-            int[] dy = new int[]{-1,0,1,0};
-            int[] dx = new int[]{0,1,0,-1};
-            boolean isPossible = false;
-            for(int i = 0 ; i < 4 ; i++){
-                int ny = r + dy[i];
-                int nx = c + dx[i];
-                if(ny >= 0 && ny < N && nx >= 0 && nx < M){
-                    if(room.get(ny).get(nx) == 0){
-                        isPossible = true;
-                        break;
-                    }
-                }
-            }
-
-            // 빈 칸 존재
-            if(isPossible){
-                // 반시계방향 회전
-                d -= 1;
-                if(d < 0){
-                    d = 3;
-                }
-                // 앞칸이 청소되지 않은 빈칸이면 전진
-                int ny = r + dy[d];
-                int nx = c + dx[d];
-                if(ny>=0&&ny<N&&nx>=0&&nx<M){
-                    if(room.get(ny).get(nx) == 0){
-                        r = ny;
-                        c = nx;
-                        continue;
-                    }
-                }
-            }
-
-            // 빈칸 존재하지 않음
-            else{
-                // 후진 가능한 경우
-                int ny = r - dy[d];
-                int nx = c - dx[d];
-                if(ny>=0&&ny<N&&nx>=0&&nx<M){
-                    if(room.get(ny).get(nx) != 1){
-                        r = ny;
-                        c = nx;
-                        continue;
-                    }
-                    else if(room.get(ny).get(nx) == 1){
-                        break;
-                    }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (clean[i][j]) {
+                    cnt++;
                 }
             }
         }
 
         System.out.println(cnt);
+
     }
+
+    static boolean range(int y, int x) {
+        return y >= 0 && y < N && x >= 0 && x < M;
+    }
+
 }
-
-
