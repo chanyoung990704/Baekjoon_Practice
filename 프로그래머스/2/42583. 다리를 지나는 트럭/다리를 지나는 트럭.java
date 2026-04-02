1dbd7bcb21e2c13dbf42bcf72fe8536c2324b921
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-02 15:20:16 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 18.05 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,43 +1,40 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         
-        Deque<Integer> running = new ArrayDeque<>();
-        Deque<Integer> trucks = new ArrayDeque<>(Arrays.stream(truck_weights).boxed()
-                                                 .collect(Collectors.toList()));
-        for(int i = 0 ; i < bridge_length ; i++){
-            running.offer(0);
-        }
+        // 큐 무게, [] 무게, 시작 시점, 현재 시간
         
         int time = 0;
-        int curWeight = 0;
-        int curSize = 0;
+        int bridge_weight=0;
+        Queue<int[]> q = new ArrayDeque<>();
+        int finished = 0;
+        int idx = 0;
         
-        while(!running.isEmpty() || !trucks.isEmpty()){
-            
-            // 러닝 큐를 한칸씩 이동
-            if(!running.isEmpty()){
-                curWeight -= running.pollFirst();
-                curSize -= 1;
+        while(true){
+            // 다 지났으면
+            if(finished == truck_weights.length){
+                break;
             }
             
-            // 무게 & 용량 확인 후 푸시
-            if(!trucks.isEmpty()){
-                int next = trucks.peekFirst();
-                if(curWeight + next <= weight && curSize + 1 <= bridge_length){
-                    curWeight += next;
-                    curSize += 1;
-                    running.offerLast(next);
-                    trucks.pollFirst();
-                }else{
-                    running.offerLast(0);
-                }
-            }
-            
+            // 시간 증가
             time++;
+            
+            // 큐에서 빠질 것들 체크
+            while(!q.isEmpty() && time - q.peek()[1] >= bridge_length){
+                bridge_weight -= q.poll()[0];
+                finished++;
+            }
+            
+            // 트럭 진입
+            if(idx < truck_weights.length && bridge_weight + truck_weights[idx] <= weight){
+                bridge_weight += truck_weights[idx];
+                q.offer(new int[]{truck_weights[idx++], time});
+            }
+            
+            
         }
+ 
         return time;
     }
 }
