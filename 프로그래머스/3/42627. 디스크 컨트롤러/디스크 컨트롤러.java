07a2f19a0b54ba45344cad1e9a806915043a8059
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-03 16:31:49 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.66 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,55 +2,43 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-
-    class Work{
-        int idx;
-        int request;
-        int time;
-        
-        Work(int idx, int request, int time){
-            this.idx = idx;
-            this.request = request;
-            this.time = time;
-        }
-    }
-    
     public int solution(int[][] jobs) {
         
-        // 대기 큐 우선순위 소요시간 -> 요청 시각 -> 번호 작은거
-        PriorityQueue<Work> pq =
-            new PriorityQueue<>(Comparator.comparing((Work w) -> w.time)
-                               .thenComparing((Work w) -> w.request)
-                               .thenComparing((Work w) -> w.idx));
-        
-        // 작업 시작
-        int time = 0;
-        List<Work> list = new ArrayList<>();
+        int[][] copied = new int[jobs.length][3];
         for(int i = 0 ; i < jobs.length ; i++){
-            list.add(new Work(i, jobs[i][0], jobs[i][1]));
+            copied[i] = new int[]{jobs[i][0], jobs[i][1], i};
         }
-        list.sort(Comparator.comparing((Work w) -> w.request));
         
-        int idx = 0;
-        int total = 0;
-        while(idx < list.size() || !pq.isEmpty()){
-            // 현재 시각 이전 작업
-            while(idx < list.size() && list.get(idx).request <= time){
-                pq.offer(list.get(idx));
+        // 정렬
+        Arrays.sort(copied, (a, b) -> a[0] - b[0]);
+        
+        // 작업 소요시간, 작업 요청 시각, 작업 번호
+        PriorityQueue<int[]> pq = 
+            new PriorityQueue<>(Comparator.comparing((int[] a) -> a[0])
+                               .thenComparing(a -> a[1])
+                               .thenComparing(a -> a[2])); 
+        
+        int answer = 0;
+        int count = 0, idx = 0, time = 0;
+        while(count < jobs.length){
+            // 현재 시각보다 같거나 짧은 거 큐에 넣기
+            while(idx < jobs.length && copied[idx][0] <= time){
+                pq.offer(new int[]{copied[idx][1], copied[idx][0], copied[idx][2]});
                 idx++;
             }
             
-            // 작업 진행
-            if(!pq.isEmpty()){
-                Work cur = pq.poll();
-                time += cur.time;
-                total += time - cur.request;
+            // 큐가 비었으면
+            if(pq.isEmpty()){
+                time = copied[idx][0];
             }else{
-                time = list.get(idx).request;
+                int[] p = pq.poll();
+                time += p[0];
+                answer += (time - p[1]);
+                count++;
             }
-
         }
         
-        return total / list.size();
+        
+        return answer / jobs.length;
     }
 }
