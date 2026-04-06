552146b7f0fbe51da2f6a91a260b338d31e556b9
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-06 14:54:31 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 412 ms, Memory: 96608 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.java
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.java
@@ -2,54 +2,54 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
     static int N;
-
-    static final int INF = 1000000000;
+    static int[][] cost;
+    static int[][] memo;
+    static final int INF = 1_000_000_000;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        cost = new int[N][N];
 
-        int[][] cost = new int[N][N];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 cost[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] dp = new int[N][1 << N];
+        // memo[사람 인덱스][현재까지의 일 배정 상태]
+        memo = new int[N][1 << N];
         for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], INF);
+            Arrays.fill(memo[i], -1);
         }
 
-        for (int i = 0; i < N; i++) {
-            dp[0][1 << i] = cost[0][i];
+        // 0번 사람부터, 아무 일도 배정되지 않은 상태(0)로 시작
+        System.out.println(solve(0, 0));
+    }
+
+    static int solve(int pIdx, int mask) {
+        // 모든 사람에게 일이 배정된 경우 (기저 조건)
+        if (pIdx == N) {
+            return 0;
         }
 
-        for (int i = 1; i < N; i++) {
-            for (int cur = 0; cur < (1 << N); cur++) {
-                // 현재 불가능 마스킹
-                if (dp[i - 1][cur] == INF) {
-                    continue;
-                }
+        // 이미 계산된 값이 있는 경우 반환 (메모이제이션)
+        if (memo[pIdx][mask] != -1) {
+            return memo[pIdx][mask];
+        }
 
-                for (int j = 0; j < N; j++) {
-                    // 다음 할당 작업
-                    if (((cur >> j) & 1) > 0) {
-                        continue;
-                    }
-                    int next = cur | (1 << j);
-                    dp[i][next] = Math.min(dp[i][next], dp[i - 1][cur] + cost[i][j]);
-                }
+        int res = INF;
+        // 현재 사람(pIdx)에게 할당할 수 있는 일(j)을 순회
+        for (int j = 0; j < N; j++) {
+            // j번째 일이 아직 배정되지 않았다면
+            if ((mask & (1 << j)) == 0) {
+                // 다음 사람으로 넘어가며 비용 합산 및 최솟값 갱신
+                res = Math.min(res, solve(pIdx + 1, mask | (1 << j)) + cost[pIdx][j]);
             }
         }
 
-        System.out.println(dp[N - 1][(1 << N) - 1]);
-
+        return memo[pIdx][mask] = res;
     }
-
 }
