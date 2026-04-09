6a1b7fbb28a2c135486a0aeb86c2bea00ebb2aba
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-09 17:18:41 +0900

    [Gold III] Title: 파일 합치기, Time: 528 ms, Memory: 31940 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,42 +1,59 @@
-import java.io.*;
 import java.util.*;
-import java.util.stream.*;
+import java.io.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int T;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
 
-
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            List<Integer> arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+            N = Integer.parseInt(br.readLine());
+            int[] nums = new int[N];
 
-            int[][] dp = new int[N][N];
-
-            List<Integer> prefix = new ArrayList<>();
-            prefix.add(0);
-            for (int n : arr) {
-                prefix.add(prefix.get(prefix.size() - 1) + n);
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                nums[i] = Integer.parseInt(st.nextToken());
             }
 
+            //누적합
+            int[] sums = new int[N+1];
+            for (int i = 1; i < sums.length; i++) {
+                sums[i] = sums[i - 1] + nums[i - 1];
+            }
 
+            // 구간 DP
+            int[][] dp = new int[N][N];
+            
+
+            // 길이 2
             for (int len = 2; len <= N; len++) {
-                for (int i = 0; i <= N - len; i++) {
+                for (int i = 0; i < N; i++) {
                     int j = i + len - 1;
+                    if (j >= N) {
+                        break;
+                    }
+
                     dp[i][j] = Integer.MAX_VALUE;
                     for (int k = i; k < j; k++) {
-                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + prefix.get(j + 1) - prefix.get(i));
+                        int val = dp[i][k] + dp[k + 1][j] + (sums[j + 1] - sums[i]);
+                        dp[i][j] = Math.min(dp[i][j], val);
                     }
                 }
-
-
             }
 
+            // 출력
             sb.append(dp[0][N - 1]).append("\n");
+
         }
-        
-        System.out.println(sb.toString());
+
+        System.out.println(sb);
+
     }
+
 }
