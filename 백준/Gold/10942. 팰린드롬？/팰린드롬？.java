c596ae4e7e4de1839971fc386894805a7d9adcce
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-09 15:50:51 +0900

    [Gold IV] Title: 팰린드롬?, Time: 776 ms, Memory: 217896 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,17 +1,24 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
 
-        int N = sc.nextInt();
-        List<Integer> nums = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            nums.add(sc.nextInt());
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        // dp
+        /////////////DP계산//////////////////
+
         boolean[][] dp = new boolean[N][N];
 
         // 길이 1
@@ -20,31 +27,37 @@ public class Main {
         }
 
         // 길이 2
-        for(int i = 0 ; i < N - 1; i++) {
-            int j = i + 1;
-            if(nums.get(i).equals(nums.get(j))) {
-                dp[i][j] = true;
+        for (int i = 0; i < N - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                dp[i][i + 1] = true;
             }
         }
 
-        // 길이 3 이상
-        for(int len = 3  ; len <= N; len++) {
-            for(int i = 0 ; i < N - len + 1; i++) {
-                int j = i + len - 1;
-                if(nums.get(i).equals(nums.get(j)) && dp[i + 1][j - 1]) {
+        for (int len = 3; len <= N; len++) {
+            for (int i = 0; i < N - len + 1; i++) {
+                int j = i + len -1;
+                if (nums[i] == nums[j] && dp[i+1][j-1]) {
                     dp[i][j] = true;
                 }
             }
         }
 
-        int M = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < M; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            sb.append(dp[s - 1][e - 1] ? 1 : 0).append("\n");
+
+        //////////////////////////////////////
+
+        int M = Integer.parseInt(br.readLine());
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken())-1;
+            int e = Integer.parseInt(st.nextToken())-1;
+
+            sb.append(dp[s][e] ? '1' : '0').append("\n");
+
         }
 
-        System.out.print(sb.toString());
+        System.out.println(sb);
+
     }
+
 }
