6798087de4a30edc8cf920e0dba5cf0f6757f791
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-13 15:49:03 +0900

    [level 3] Title: 거스름돈, Time: 21.39 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,17 +1,18 @@
-import java.util.*;
-
 class Solution {
+    int MOD = 1_000_000_007;
+    
     public int solution(int n, int[] money) {
-        // dp
         
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n+1];
         dp[0] = 1;
         
-        int mod = 1000000007;
-        
         for(int m : money){
-            for(int i = m ; i <= n ; i++){
-                dp[i] = (dp[i] % mod + dp[i - m] % mod) % mod;
+            for(int i = 0 ; i <= n ; i++){
+                if(i < m){
+                    continue;
+                }
+                
+                dp[i] = (dp[i] + dp[i-m]) % MOD;
             }
         }
         
