27b8efd63f94bae28a47d558d3cd4341086c6762
CYPARK <80041365+chanyoung990704@users.noreply.github.com>
2026-04-15 17:41:54 +0900

    [level 1] Title: 실패율, Time: 176.62 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,41 +1,29 @@
 import java.util.*;
 import java.util.stream.*;
 
+
 class Solution {
-    
-    class StageInfo{
-        double failure;
-        int idx;
-        
-        StageInfo(double failure, int idx){
-            this.failure = failure;
-            this.idx = idx;
-        }
-        
-        public double getFailure(){
-            return this.failure;
-        }
-    
-        public int getIdx(){
-            return this.idx;
-        }
-        
-    }
-    
     public int[] solution(int N, int[] stages) {
-        return IntStream.range(1, N + 1)
-            .mapToObj(i -> {
-                long totalPlayers = Arrays.stream(stages)
-                    .filter(j -> j >= i).count();
-                long failedPlayers = Arrays.stream(stages)
-                    .filter(j -> j == i).count();
-                double failureRate = totalPlayers > 0 ?
-                    (double) failedPlayers / totalPlayers : 0;
-                return new StageInfo(failureRate, i);
-            })
-            .sorted(Comparator.comparing(StageInfo::getFailure).reversed()
-                   .thenComparing(Comparator.comparing(StageInfo::getIdx)))
-            .mapToInt(StageInfo::getIdx)
+        
+        // 스테이지 도달 및 완료
+        int[] cur = new int[N+2];
+        int[] fin = new int[N+2];
+        
+        for(int s : stages){
+            for(int j = 1 ; j <= s ; j++){
+                cur[j]++;
+                if(s > j){
+                    fin[j]++;
+                }
+            }
+        }
+        
+        return IntStream.range(1, N+1)
+            .boxed() 
+            .sorted(Comparator.comparingDouble(i -> (double) fin[i] / cur[i]))
+            .mapToInt(Integer::intValue)
             .toArray();
+        
+        
     }
 }
